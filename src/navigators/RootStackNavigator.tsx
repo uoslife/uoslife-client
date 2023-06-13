e604f2890c83c5261d6b1478de15b99d37f9c5a9
Junsu Lee <me@adrinerdp.co>
2023-06-13 12:07:24 +0900

    feat: react-native-webview 추가

--- src/navigators/RootStackNavigator.tsx
+++ src/navigators/RootStackNavigator.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import PoCScreen from '../screens/PoCScreen';
+import WebViewScreen from '../screens/WebViewScreen';
 
 export type RootStackParamList = {
   PoC: undefined;
+  WebView: {url?: string};
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -11,6 +13,7 @@ const Stack = createStackNavigator<RootStackParamList>();
 const RootStackNavigator: React.FC = () => (
   <Stack.Navigator initialRouteName="PoC" screenOptions={{headerShown: false}}>
     <Stack.Screen name="PoC" component={PoCScreen} />
+    <Stack.Screen name="WebView" component={WebViewScreen} />
   </Stack.Navigator>
 );
 
