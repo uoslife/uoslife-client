3af953855aaa66b2028d4c2b555c238791bfd851
은배 <geb9211@gmail.com>
2023-08-02 09:52:34 +0800

    feat: account screen 작성

---
+++ src/screens/account/PortalAuthenticationScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const PortalAuthenticationScreen = () => {
+  return (
+    <View>
+      <Text>PortalAuthenticationScreen</Text>
+    </View>
+  );
+};
+
+export default PortalAuthenticationScreen;
---
+++ src/screens/account/index.ts
@@ -0,0 +1,5 @@
+export {default as SignupScreen} from './SignupScreen';
+export {default as AgreementTermsScreen} from './AgreementTermsScreen';
+export {default as PortalAuthenticationScreen} from './PortalAuthenticationScreen';
+export {default as SetNicknameScreen} from './SetNicknameScreen';
+export {default as VerificationScreen} from './VerificationScreen';
