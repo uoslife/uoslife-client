c6212d6f6369ae34d6ec12c8a6ab25ce8e383579
youngchan Kim <aacz12035@gmail.com>
2023-08-05 01:50:48 +0900

    Revert "feat: roundInput Type 추가"
    This reverts commit 8b8c4005c845639aa0c6f14dfa24278556217c93.

--- src/components/textFields/roundInput/RoundInput.tsx
+++ src/components/textFields/roundInput/RoundInput.tsx
@@ -1,59 +1,20 @@
 import React from 'react';
-import {Pressable, TextInput} from 'react-native';
+import {TextInput} from 'react-native';
 
 import styled from '@emotion/native';
 
 import RoundInputProps from './RoundInput.type';
-import roundInputType from './RoundInput.type';
 
-const RoundInput = ({
-  keyboardType = 'default',
-  status = 'default',
-  value,
-  placeholder,
-  children,
-  onChangeText,
-  ...props
-}: RoundInputProps) => {
+const RoundInput = ({placeholder}: RoundInputProps) => {
   return (
-    <Pressable>
-      <S.roundInputContainer status={status}>
-        <TextInput
-          keyboardType={keyboardType}
-          placeholder={placeholder}
-          value={value}
-          onChangeText={onChangeText}
-          {...props}
-        />
-        {children}
-      </S.roundInputContainer>
-    </Pressable>
+    <S.roundInputContainer>
+      <TextInput placeholder={placeholder} />
+    </S.roundInputContainer>
   );
 };
 
 export default RoundInput;
 
-const getBorderColor = (status: string) => {
-  switch (status) {
-    case 'success':
-      return 'blue';
-    case 'error':
-      return 'red';
-    default:
-      return 'black';
-  }
-};
-
 const S = {
-  roundInputContainer: styled.View<roundInputType>`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    height: 62px;
-    border-radius: 15px;
-    border: 1px solid ${({status}) => getBorderColor(status!)};
-    padding: 19px;
-  `,
+  roundInputContainer: styled.View``,
 };
--- src/components/textFields/roundInput/RoundInput.type.ts
+++ src/components/textFields/roundInput/RoundInput.type.ts
@@ -1,12 +1,5 @@
-import {TextInputProps, KeyboardTypeOptions} from 'react-native';
-
-export type RoundInputProps = TextInputProps & {
-  keyboardType?: KeyboardTypeOptions;
-  onChangeText?: (value: string, target?: string) => void;
-  placeholder?: string;
-  status?: 'default' | 'error' | 'success';
-  value?: string;
-  children?: React.ReactNode;
+export type RoundInputProps = {
+  placeholder: string;
 };
 
 export default RoundInputProps;
