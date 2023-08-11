0735626871e9e33c8163e54bec120afe8398bd0d
John Doeexit <2002eojin@gmail.com>
2023-08-11 16:35:21 +0900

    feat: SecondPage 디자인에 맞춰 중복확인 버튼 삭제, 로직 변경

--- src/screens/account/AgreementTerms/SecondPage.tsx
+++ src/screens/account/AgreementTerms/SecondPage.tsx
@@ -39,34 +39,35 @@ interface SecondPageProps {
 
 interface InputProps {
     text: string;
-    isValid: boolean;
-    isVerified: boolean;
+    isChecked: boolean;
 }
 
 const SecondPage = ({ setPage }: SecondPageProps) => {
     const [input, setInput] = useState<InputProps>({
         text: "",
-        isValid: false, // 정규표현식 일치 여부
-        isVerified: false, // 중복확인 여부
+        isChecked: false,
     });
-    const [isBottomSheetOpened, setIsBottomSheetOpened] = useState(false);
-    const [isModalOpened, setIsModalOpened] = useState(false);
+    const [isBottomSheetOpened, setIsBottomSheetOpened] = useState<boolean>(false);
+    const [isModalOpened, setIsModalOpened] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState<string>("");
 
     const onChange = (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
         const text = e.nativeEvent.text;
 
         const regex = /^[가-힣a-zA-Z0-9!@#$%^&*()-_+=~]{1,8}$/;
-        const isValid = regex.test(text);
 
-        const isVerified = false;
+        const isValid = regex.test(text); // 정규표현식 일치 여부 확인
+        let isVerified = false; // 중복되지 않았는지 확인: API 붙인 뒤 로직 작성
 
-        setInput({ text, isValid, isVerified })
-    }
-
-    const verifyDuplicate = () => {
-        if (true) {
-            setInput({ ...input, isVerified: true })
+        try {
+            isVerified = true;
+        } catch (err) {
+            console.log(err);
         }
+
+        const isChecked = isValid && isVerified;
+
+        setInput({ text, isChecked })
     }
 
     return (
@@ -75,18 +76,20 @@ const SecondPage = ({ setPage }: SecondPageProps) => {
                 <S.contentsContainer>
                     <S.description1>사용하실 닉네임을 입력해주세요.</S.description1>
                     <View>
+                        <S.description2>닉네임은 최대 8자로 설정 가능합니다.</S.description2>
+                        <S.description2>한글, 영문, 숫자, 특수기호를 이용해주세요.</S.description2>
+                    </View>
+                    <View>
+                        <View><S.literalNickname>닉네임</S.literalNickname></View>
                         <S.nicknameAndCheck>
                             <TextInput value={input.text} onChange={onChange} style={{ borderColor: "black", borderStyle: 'solid', borderWidth: 5, flex: 1 }} />
-                            <View style={{ width: 100 }}><Button onPress={verifyDuplicate} type={input.isValid ? "primary" : "default"} label={'중복확인'}></Button></View>
                         </S.nicknameAndCheck>
-                        <S.description2>&#8226; 한글, 영문, 숫자, 특수기호를 이용하여</S.description2>
-                        <S.description2>최대 8자 설정 가능합니다.</S.description2>
                     </View>
                 </S.contentsContainer>
                 <Button
                     label={"확인"}
                     onPress={() => { setIsBottomSheetOpened(true) }}
-                    type={input.isVerified ? "primary" : "default"}
+                    type={input.isChecked ? "primary" : "default"}
                 />
             </S.pageWrapper>
             {isBottomSheetOpened && <BottomSheet>
@@ -109,12 +112,11 @@ const S = {
         flex: 1;
         display: flex;
         flex-direction: column;
-        align-items: center;
 
-        padding-top: 42px;
-        padding-right: 21px;
-        padding-left: 21px;
-        padding-bottom: 27px;
+        padding-top: 28px;
+        padding-right: 16px;
+        padding-left: 16px;
+        padding-bottom: 28px;
         justify-content: space-between;
     `,
     nicknameAndCheck: styled.View`
@@ -125,7 +127,7 @@ const S = {
     contentsContainer: styled.View`
         display: flex;
         flex-direction: column;
-        gap: 24px;
+        gap: 32px;
     `,
     description1: styled.Text`
         font-size: 25px;
@@ -134,31 +136,16 @@ const S = {
     `,
     description2: styled.Text`
         font-size: 18px;
-        padding-left: 27px;
-        padding-right: 27px;
+        color: black;
     `,
     inputContainer: styled.View`
         display: flex;
         flex-direction: column;
         gap: 28px;
     `,
-    // cardContainer: styled.View`
-    //   padding: 12px;
-    //   width: 200px;
-    //   display: flex;
-    //   flex-direction: column;
-    //   border-radius: 8px;
-    //   background: #efefef;
-    // `,
-    // cardWrapper: styled.View`
-    //   display: flex;
-    //   flex-direction: row;
-    //   justify-content: space-between;
-    // `,
-    // button: styled.View`
-    //   border-radius: 16px;
-    //   background: #d0d0d0;
-    //   padding: 4px 10px;
-    //   font-size: 10px;
-    // `,
+    literalNickname: styled.Text`
+        padding-left: 12px;
+        padding-right: 12px
+        color: black;
+    `
 };
