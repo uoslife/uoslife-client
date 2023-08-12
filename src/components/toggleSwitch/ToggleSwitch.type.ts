ee515a419b25fe4b7cd80f5b37cd1b06481abfc0
Song Chaehee <sch003322@gmail.com>
2023-08-13 00:50:16 +0900

    fix: optional type 지정 수정

--- src/components/toggleSwitch/ToggleSwitch.type.ts
+++ src/components/toggleSwitch/ToggleSwitch.type.ts
@@ -1,7 +1,7 @@
 export type ToggleSwitchProps = {
   isOn: boolean;
   size: 'small' | 'medium';
-  onToggle?: (name: string, value?: boolean) => void;
+  onToggle?: (name?: string, value?: boolean) => void;
 };
 
 export default ToggleSwitchProps;
