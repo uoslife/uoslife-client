77da3583de3feb59609ac05c4fc0dd6200c75e45
John Doeexit <2002eojin@gmail.com>
2023-08-12 15:05:32 +0900

    refactor: 모달에 들어갈 컴포넌트 분리

--- src/screens/account/AgreementTerms/SecondPage.tsx
+++ src/screens/account/AgreementTerms/SecondPage.tsx
@@ -1,97 +1,12 @@
 import styled from '@emotion/native';
-import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
-import { View, Text, NativeSyntheticEvent, TextInputChangeEventData, Image } from 'react-native';
-import { BaseButton, TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import React, { Dispatch, SetStateAction, useState } from 'react'
+import { View, NativeSyntheticEvent, TextInputChangeEventData, Image } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 import { Button } from '../../../components/button/Button';
 import BottomSheet from '../../../components/modals/BottomSheet';
 import PopUp from '../../../components/modals/PopUp';
-
-// 약관 동의: 하단 팝업에 들어갈 내용
-// 재사용성은 고려하지 않고 짬, 리팩토링 굳이 필요하려나?
-const AgreementToTerms = ({ openPopUp }: { openPopUp: () => void }) => {
-    const [checked, setChecked] = useState<boolean[]>([false, false, false]);
-
-    // const checkCheckedPath = "../../../assets/images/check_checked.png";
-    // const checkUncheckedPath = "../../../assets/images/check_unchecked.png";
-    // const checkCircleCheckedPath = "../../../assets/images/check_circle_checked.png";
-    // const checkCircleUncheckedPath = "../../../assets/images/check_circle_unchecked.png";
-
-    const CheckCheckedImage = () => <Image source={require("../../../assets/images/check_checked.png")} />
-    const CheckUncheckedImage = () => <Image source={require("../../../assets/images/check_unchecked.png")} />
-    const CheckCircleCheckedImage = () => <Image source={require("../../../assets/images/check_circle_checked.png")} />
-    const CheckCircleUncheckedImage = () => <Image source={require("../../../assets/images/check_circle_unchecked.png")} />
-
-    useEffect(() => { if (checked[2]) openPopUp() }, [checked[2]]);
-
-    const checkedAll = checked[0] && checked[1] && checked[2];
-    const checkedRequired = checked[0] && checked[1];
-
-    const toggleAll = () => {
-        if (checkedAll) {
-            setChecked([false, false, false]);
-        }
-        else
-            setChecked([true, true, true]);
-    }
-
-    const toggleSingle = (num: number) => {
-        const copied = [...checked];
-        copied[num] = !copied[num];
-
-        setChecked(copied);
-    };
-
-    return <View>
-        <View>
-            <S.checkAllBtn onPress={toggleAll}>
-                {
-                    (checkedAll)
-                        ? <CheckCircleCheckedImage />
-                        : <CheckCircleUncheckedImage />
-                }
-                <Text>약관에 모두 동의</Text>
-            </S.checkAllBtn>
-            <S.checkSingleContainer>
-                <TouchableOpacity onPress={() => { toggleSingle(0) }}>
-                    {checked[0] ? <CheckCheckedImage /> : <CheckUncheckedImage />}
-                </TouchableOpacity>
-                <Text onPress={() => { }}>[필수] 개인정보처리방침</Text>
-            </S.checkSingleContainer>
-            <S.checkSingleContainer>
-                <TouchableOpacity onPress={() => { toggleSingle(1) }}>
-                    {checked[1] ? <CheckCheckedImage /> : <CheckUncheckedImage />}
-                </TouchableOpacity>
-                <Text onPress={() => { }}>[필수] 시대생 이용약관</Text>
-            </S.checkSingleContainer>
-            <S.checkSingleContainer>
-                <TouchableOpacity onPress={() => { toggleSingle(2) }}>
-                    {checked[2] ? <CheckCheckedImage /> : <CheckUncheckedImage />}
-                </TouchableOpacity>
-                <View>
-                    <Text>[선택] 광고 및 마케팅 수신 동의 알림</Text>
-                    <Text>각종 시대생 소식 및 이벤트에 대한 정보를 제공합니다.</Text>
-                </View>
-            </S.checkSingleContainer>
-        </View>
-        <Button label='확인' type={checkedRequired ? "primary" : "default"} />
-    </View>
-}
-
-// 광고성 정보 수신동의 처리 결과: 모달에 들어갈 내용
-const AgreementProcessResult = () => (
-    <View>
-        <Text>광고성 정보 수신동의 처리 결과</Text>
-        <View>
-            <Text>전송자: UOS LIFE</Text>
-            <Text>일시: {new Date().toDateString()}</Text>
-            <Text>내용: 수신동의 처리 완료</Text>
-        </View>
-        <View>
-            <Text>광고성 정보 수신 동의는</Text>
-            <Text>MY Page &lt; [앱 설정]에서 변경 가능합니다.</Text>
-        </View>
-    </View>
-);
+import AgreementToTerms from '../../../components/contents/AgreementToTerms';
+import AgreementProcessResult from '../../../components/contents/AgreementProcessResult';
 
 interface SecondPageProps {
     setPage: Dispatch<SetStateAction<number>>
@@ -171,7 +86,9 @@ const SecondPage = ({ setPage }: SecondPageProps) => {
             {isBottomSheetOpened && <BottomSheet zIndex={1} onBackgroundPress={closeBottomSheet} darkBackground={!isPopUpOpened}>
                 <AgreementToTerms openPopUp={openPopUp} />
             </BottomSheet>}
-            {isPopUpOpened && <PopUp onClose={closePopUp} zIndex={3} darkBackground><AgreementProcessResult /></PopUp>}
+            {isPopUpOpened && <PopUp onClose={closePopUp} zIndex={3} darkBackground>
+                <AgreementProcessResult />
+            </PopUp>}
         </>
     );
 }
@@ -218,21 +135,5 @@ const S = {
         padding-left: 12px;
         padding-right: 12px
         color: black;
-    `,
-    checkSingleContainer: styled.View`
-        display: flex;
-        flex-direction: row;
-        gap: 8px;
-        width: 100%;
-        padding: 12px;
-    `,
-    checkAllBtn: styled.TouchableOpacity`
-        display: flex;
-        flex-direction: row;
-        gap: 8px;
-        width: 100%;
-        padding: 12px;
-        border-radius: 12px;
-        border: #A19F9D
     `,
 };
