11148fabc60e4df80de47d9955e6a0408eea3267
youngchan Kim <aacz12035@gmail.com>
2023-08-17 18:27:39 +0900

    refactor: 일부 변수명 type 수정

--- src/components/forms/roundTextInput/RoundTextInput.type.ts
+++ src/components/forms/roundTextInput/RoundTextInput.type.ts
@@ -3,8 +3,10 @@ import {TextInputProps, KeyboardTypeOptions} from 'react-native';
 export type RoundTextInputProps = TextInputProps & {
   keyboardType?: KeyboardTypeOptions;
   onChangeText?: (value: string, target?: string) => void;
+  onPress?: () => void;
   placeholder?: string;
   placeholderTextColor?: string;
+  label?: string;
   status?: 'default' | 'error' | 'success';
   statusMessage?: string;
   value?: string;
