50aa6a311f87e7c40dd1059c9b43df63d5b6fb5a
youngchan Kim <aacz12035@gmail.com>
2023-08-17 18:27:23 +0900

    refactor: 일부 변수명 및 css 수정

--- src/components/forms/roundTextInput/RoundTextInput.tsx
+++ src/components/forms/roundTextInput/RoundTextInput.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import {Pressable, Text, TextInput, View} from 'react-native';
-
+import {Pressable, StyleSheet, View} from 'react-native';
 import styled from '@emotion/native';
-
 import RoundTextInputProps from './RoundTextInput.type';
 import {colors, typographs} from '@uoslife/design-system';
 
 const RoundTextInput = ({
   keyboardType = 'default',
   status = 'default',
+  label,
   statusMessage,
   value,
   placeholder,
   placeholderTextColor = colors.grey60,
   onChangeText,
+  onPress,
   children,
   ...props
 }: RoundTextInputProps) => {
   return (
     <View>
+      <S.label status={status}>{label}</S.label>
       <Pressable>
         <S.roundInputContainer value={value} status={status}>
           <S.textInput
@@ -63,8 +64,17 @@ const getStatusColor = (status: string) => {
 };
 
 const S = {
+  label: styled.Text<RoundTextInputProps>`
+    padding-left: 12px;
+    color: ${({status}) => getStatusColor(status!)};
+    ${() => typographs.bodyMedium}
+  `,
   roundInputContainer: styled.View<RoundTextInputProps>`
     position: relative;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
     width: 100%;
     padding: 5px 12px 8px 12px;
     border-bottom-width: ${({value}) => (value! ? '2px' : '1px')};
