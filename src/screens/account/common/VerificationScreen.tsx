12cdb04e3fff750091abd55e3c25b30bb6dd2cc5
youngchan Kim <aacz12035@gmail.com>
2023-08-21 15:44:37 +0900

    feat: verificationScreen 디자인 제작

--- src/screens/account/common/VerificationScreen.tsx
+++ src/screens/account/common/VerificationScreen.tsx
@@ -1,17 +1,16 @@
 import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import Header from '../../../components/header/Header';
 import styled from '@emotion/native';
-
 import Input from '../../../components/forms/input/Input';
-import {Button} from '../../../components/button/Button';
+import {Button, Txt, Timer, colors} from '@uoslife/design-system';
 import {useSetAtom} from 'jotai';
 import {accountStatusAtom} from '..';
 
 const VerificationScreen = () => {
   const setAccountStatus = useSetAtom(accountStatusAtom);
   const [inputValue, setInputValue] = useState('');
-  const [warningStatus, setWarningStatus] = useState('codeError');
+  const [warningStatus, setWarningStatus] = useState('');
   const [isVerificationCodeSent, setIsVerificationCodeSent] = useState(false);
 
   const handleWarningMessage = (status: string) => {
@@ -22,6 +21,8 @@ const VerificationScreen = () => {
         return '1일 인증 요청 가능 횟수를 초과하였습니다.';
       case 'timeExpired':
         return '요청된 시간이 만료되었습니다.';
+      default:
+        return '인증번호가 오지 않나요?';
     }
   };
 
@@ -30,11 +31,15 @@ const VerificationScreen = () => {
   };
 
   const handleRequestVerifyCode = () => {
-    if (inputValue.length < 13) return;
+    if (inputValue.length < 11) return;
     setIsVerificationCodeSent(true);
     setInputValue('');
   };
-  const handleVerifyIdentify = () => {
+
+  const handleButtonIsEnable = () => {
+    if (!isVerificationCodeSent) return inputValue.length === 11;
+  };
+  const handleVerifyIdentify = async () => {
     setAccountStatus(prev => {
       return {
         ...prev,
@@ -48,36 +53,69 @@ const VerificationScreen = () => {
 
   return (
     <S.screenContainer>
-      <Header label="전화번호 본인인증" />
+      <Header label={'전화번호 본인인증'} />
       <S.verificationContainer>
-        <View>
-          <Text>전화번호를 입력해주세요</Text>
-          <Text>* 인증번호 전송은 1일 5회로 제한됩니다.</Text>
+        <View style={{gap: 32}}>
+          <View style={{gap: 8}}>
+            <Txt
+              typograph={'headlineMedium'}
+              color={'grey190'}
+              label={
+                isVerificationCodeSent
+                  ? '인증번호를 입력해주세요.'
+                  : '전화번호를 입력해주세요.'
+              }
+            />
+            <Txt
+              typograph={'bodyMedium'}
+              color={'grey190'}
+              label={'인증번호 전송은 1일 5회로 제한됩니다.'}
+            />
+          </View>
           <Input
             onChangeText={text => onChangeText(text)}
-            maxLength={11}
+            maxLength={isVerificationCodeSent ? 6 : 11}
             onPress={() => setInputValue('')}
             keyboardType={'numeric'}
             value={inputValue}
-            label={'안녕'}
+            label={isVerificationCodeSent ? '인증번호' : '전화번호'}
             statusMessage={handleWarningMessage(warningStatus)}
             status={warningStatus ? 'error' : 'default'}
-            placeholder={
-              isVerificationCodeSent ? '인증번호 입력' : '01012345678'
-            }
-          />
+            placeholder={isVerificationCodeSent ? '000000' : '01012345678'}>
+            {isVerificationCodeSent && (
+              <>
+                <Timer
+                  style={{
+                    position: 'absolute',
+                    top: 10,
+                    right: inputValue ? 50 : 7,
+                  }}
+                />
+                <S.requestRetryButton
+                  style={{
+                    borderBottomColor: colors.grey190,
+                    borderBottomWidth: 1,
+                  }}>
+                  <Txt
+                    label={'재전송'}
+                    color={'grey190'}
+                    typograph={'labelMedium'}
+                  />
+                </S.requestRetryButton>
+              </>
+            )}
+          </Input>
         </View>
-        <View>
-          <Button
-            type={inputValue.length > 12 ? 'primary' : 'default'}
-            label={isVerificationCodeSent ? '인증번호 받기' : '본인인증 하기'}
-            onPress={
-              isVerificationCodeSent
-                ? handleVerifyIdentify
-                : handleRequestVerifyCode
-            }
-          />
-        </View>
+        <Button
+          label={isVerificationCodeSent ? '인증번호 받기' : '본인인증 하기'}
+          onPress={
+            isVerificationCodeSent
+              ? handleVerifyIdentify
+              : handleRequestVerifyCode
+          }
+          isEnabled={!isVerificationCodeSent && inputValue.length === 11}
+          isFullWidth={true}
+        />
       </S.verificationContainer>
     </S.screenContainer>
   );
@@ -85,22 +123,21 @@ const VerificationScreen = () => {
 
 const S = {
   screenContainer: styled.View`
-    height: 100%;
-    width: 100%;
+    flex: 1;
   `,
 
   verificationContainer: styled.View`
-    display: flex;
     flex: 1;
     flex-direction: column;
     justify-content: space-between;
     padding: 42px 28px;
   `,
 
-  retryCodeRequestContainer: styled.View`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+  requestRetryButton: styled.View`
+    position: absolute;
+    top: 45px;
+    right: 7px;
+    padding-bottom: 1px;
   `,
 };
 
