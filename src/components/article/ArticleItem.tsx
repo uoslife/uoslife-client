705414fd4cc3d557668aa778634af48eabe03164
Limeojin <2002eojin@gmail.com>
2023-08-24 02:55:38 +0900

    [APP-36] 공지사항 첫화면 구현 (#35)
    # Summary
    - 공지사항 페이지에서 작업할 수 있도록 임시로 navigator를 설정했습니다.
    - API를 붙이기 전에 이용할 게시글 더미 데이터와 관련 타입들을 작성했습니다.
    - 게시글 메뉴 전환에 이용할 MenuTab 컴포넌트를 작성했습니다.
    - 게시글 리스트를 보여줄 ArticleList, 그에 들어갈 ArticleItem 컴포넌트를 작성했습니다.
    
    # To-do
    - (API 달 때) 더미 데이터를 실 데이터로 교체해야 합니다.
    - (API 달 때) ArticleItem에서 description 영역과 bookmark 영역에 달려 있는 onPress
    listener를 교체해야 합니다.
    - (안드로이드) 상단 헤더가 잘리는 이슈가 있어 해결이 필요합니다.
    
    ---------
    
    Co-authored-by: EunBae Gong <76601773+eunbae0@users.noreply.github.com>

---
+++ src/components/article/ArticleItem.tsx
@@ -0,0 +1,92 @@
+import styled from '@emotion/native';
+import {Txt} from '@uoslife/design-system';
+import React from 'react';
+import {Image, Text} from 'react-native';
+
+// 임시타입이라 따로 빼놓지는 않겠습니다
+type Article = {
+  bookmarkCnt: number;
+  bookmarkByMe: boolean;
+  title: string;
+  category: string; // XX과
+  uploadTime: Date;
+};
+
+const ArticleItem = ({article}: {article: Article}) => {
+  const {bookmarkCnt, category, title, uploadTime, bookmarkByMe} = article;
+
+  const BookmarkToggleOn = () => (
+    <Image source={require('../../assets/images/bookmark_toggle_on.png')} />
+  );
+  const BookmarkToggleOff = () => (
+    <Image source={require('../../assets/images/bookmark_toggle_off.png')} />
+  );
+
+  const pad = (num: number) => (num < 10 ? `0${num}` : num);
+  const uploadYear = uploadTime.getFullYear();
+  const uploadMonth = pad(uploadTime.getMonth());
+  const uploadDay = pad(uploadTime.getDay());
+  const uploadHours = pad(uploadTime.getHours());
+  const uploadMinutes = pad(uploadTime.getMinutes());
+  const uploadDate = pad(uploadTime.getDate());
+  const processedDateString =
+    uploadDate == new Date().getDate()
+      ? `${uploadHours}:${uploadMinutes}`
+      : `${uploadYear}.${uploadMonth}.${uploadDay}`;
+  const processedCategoryAndDate = `${category} | ${processedDateString}`;
+
+  //  bookmark toggle
+  const onPressBookmark = () => {};
+
+  // 링크 클릭시 페이지 이동
+  const onPressArticleLink = () => {};
+
+  return (
+    <S.articleItemWrapper>
+      <S.description onPress={onPressArticleLink}>
+        <Txt color="black" typograph="bodyMedium" label={title} />
+        <Txt
+          color="grey90"
+          typograph="labelSmall"
+          label={processedCategoryAndDate}
+        />
+      </S.description>
+
+      <S.bookmarkContainer onPress={onPressBookmark}>
+        {bookmarkByMe ? <BookmarkToggleOn /> : <BookmarkToggleOff />}
+        <Text>{bookmarkCnt}</Text>
+      </S.bookmarkContainer>
+    </S.articleItemWrapper>
+  );
+};
+
+const S = {
+  articleItemWrapper: styled.View`
+    padding-left: 16px;
+    padding-right: 16px;
+    padding-bottom: 8px;
+    padding-top: 8px;
+
+    width: 100%;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  `,
+  description: styled.Pressable`
+    flex: 1;
+
+    display: flex;
+    gap: 4px;
+  `,
+  bookmarkContainer: styled.Pressable`
+    display: flex;
+    align-items: center;
+    justify-contents: center;
+    width: 48px;
+    height: 60px;
+  `,
+  textContainer: styled.View``,
+};
+
+export default ArticleItem;
