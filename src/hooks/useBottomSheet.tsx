e22cb923e000ad810764ca9f2b6998d5c38aa89c
limeojin363 <2002eojin@gmail.com>
2023-08-24 04:21:48 +0900

    feat: BottomSheet hooks로 병합, 확정된 디자인에 맞춰 구현

--- src/hooks/useBottomSheet.tsx
+++ src/hooks/useBottomSheet.tsx
@@ -1,36 +1,86 @@
 import React, {useState, useEffect} from 'react';
-import BottomSheet from '../components/overlay/BottomSheet';
 import styled from '@emotion/native';
+import {Image, View} from 'react-native';
+import {Button, Txt, colors} from '@uoslife/design-system';
 
-// 배경을 누르면 사라집니다..
-const useBottomSheet = () => {
+type BottomSheetOption = {
+  text: string;
+  description?: string;
+  onPressArrow: () => void;
+  toggleChecked: () => void;
+  checked: boolean;
+};
+
+type BottomSheet = {
+  withHandleBar?: true;
+  options: BottomSheetOption[];
+  onPressBtn: () => void;
+  btnEnabled: boolean;
+};
+
+// 모달은 항상 바텀시트보다 위에 있습니다
+// 배경을 누르면 사라집니다(기본값). setModalBgOnpress를 통해 수정 가능합니다.
+const useBottomSheet = ({
+  onPressBtn,
+  options,
+  withHandleBar,
+  btnEnabled,
+}: BottomSheet) => {
   const [bottomSheetOpened, setBottomSheetOpened] = useState<boolean>(false);
-  const [bottomSheetContent, setBottomSheetContent] =
-    useState<React.ReactElement>(<></>);
-  const [bottomSheetZIndex, setBottomSheetZIndex] = useState<number>(5);
   const [bottomSheetBgDark, setBottomSheetBgDark] = useState<boolean>(true);
 
-  const [bottomSheetBgOnpress, setBottomSheetBgOnpress] = useState<() => void>(
-    () => {},
+  // 아이콘: 라이브러리의 것으로 대체 필요
+  const RightArrowIcon = () => (
+    <Image source={require('../assets/images/right_arrow.png')} />
+  );
+  const CheckUncheckedImage = () => (
+    <Image source={require('../assets/images/check_unchecked.png')} />
   );
 
-  const openBottomSheet = () => {
-    setBottomSheetOpened(true);
+  const BottomSheetContent = () => {
+    return (
+      <>
+        <S.options>
+          {options.map((option, i) => (
+            <S.optionContainer
+              borderColor={colors.grey40}
+              isFirst={i === 0}
+              key={i}>
+              {/* 아이콘 대체 전 임시로 */}
+              <View style={{padding: 8}}>
+                <CheckUncheckedImage />
+              </View>
+              <S.optionDetail>
+                <S.optionTextsContainer>
+                  <Txt
+                    label={option.text}
+                    color="grey190"
+                    typograph="bodyLarge"
+                  />
+                  {option?.description && (
+                    <Txt
+                      label={option.description}
+                      color="grey130"
+                      typograph="bodySmall"
+                    />
+                  )}
+                </S.optionTextsContainer>
+                <RightArrowIcon />
+              </S.optionDetail>
+            </S.optionContainer>
+          ))}
+        </S.options>
+        <S.btnContainer>
+          <Button
+            size={'large'}
+            isEnabled={btnEnabled}
+            isFullWidth
+            label={'Button'}
+          />
+        </S.btnContainer>
+      </>
+    );
   };
-  const closeBottomSheet = () => {
-    setBottomSheetOpened(false);
-  };
-  const activateBottomSheetBgDark = () => {
-    setBottomSheetBgDark(true);
-  };
-  const deactivateBottomSheetBgDark = () => {
-    setBottomSheetBgDark(false);
-  };
-
-  // 배경을 누르면 사라지도록 초기화
-  useEffect(() => {
-    setBottomSheetBgOnpress(() => closeBottomSheet);
-  }, []);
 
   return {
     renderBottomSheet: () =>
@@ -38,21 +88,28 @@ const useBottomSheet = () => {
         <S.bottomSheetWrapper>
           <S.bottomSheetBg
             bgDark={bottomSheetBgDark}
-            zIndex={bottomSheetZIndex}
-            onPress={bottomSheetBgOnpress}
+            zIndex={5}
+            onPress={() => {
+              setBottomSheetOpened(false);
+            }}
           />
-          <BottomSheet zIndex={bottomSheetZIndex}>
-            {bottomSheetContent}
-          </BottomSheet>
+          <S.bottomSheetContainer zIndex={5}>
+            <BottomSheetContent />
+          </S.bottomSheetContainer>
         </S.bottomSheetWrapper>
       ),
-    openBottomSheet,
-    closeBottomSheet,
-    setBottomSheetContent,
-    setBottomSheetZIndex,
-    activateBottomSheetBgDark,
-    deactivateBottomSheetBgDark,
-    setBottomSheetBgOnpress,
+    openBottomSheet: () => {
+      setBottomSheetOpened(true);
+    },
+    closeBottomSheet: () => {
+      setBottomSheetOpened(false);
+    },
+    activateBottomSheetBgDark: () => {
+      setBottomSheetBgDark(true);
+    },
+    deactivateBottomSheetBgDark: () => {
+      setBottomSheetBgDark(false);
+    },
   };
 };
 
@@ -79,6 +136,62 @@ const S = {
     background-color: ${({bgDark}) =>
       bgDark ? 'rgba(0, 0, 0, 0.32)' : 'rgba(0, 0, 0, 0)'};
     z-index: ${({zIndex}) => zIndex};
+  `,
+  bottomSheetContainer: styled.View<{
+    zIndex: number;
+  }>`
+    background-color: white;
+    width: 100%;
+
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+
+    z-index: ${({zIndex}) => zIndex};
+  `,
+  options: styled.View`
+    padding-left: 16px;
+    padding-right: 16px;
+    padding-top: 12px;
+
+    display: flex;
+    gap: 8px;
+  `,
+  optionContainer: styled.View<{isFirst?: boolean; borderColor: string}>`
+    display: flex;
+    flex-direction: row;
+    gap: 4px;
+
+    padding-left: 8px;
+    padding-right: 16px;
+    padding-top: 8px;
+    padding-bottom: 8px;
+
+    width: 100%;
+
+    ${({isFirst, borderColor}) =>
+      isFirst
+        ? `margin-bottom: 8px; border-bottom-width: 1px; border-color: ${borderColor}`
+        : ''}
+  `,
+  optionDetail: styled.Pressable`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    gap: 4px;
+
+    padding-top: 8px;
+    padding-bottom: 8px;
+
+    flex: 1;
+  `,
+  optionTextsContainer: styled.View`
+    display: flex;
+    gap: 4px;
+  `,
+  btnContainer: styled.View`
+    padding: 8px 16px 8px 16px;
+    width: 100%;
   `,
 };
 
