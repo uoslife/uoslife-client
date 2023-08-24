f09a97c934cf07525ee1479cb750b8a766016a7c
limeojin363 <2002eojin@gmail.com>
2023-08-25 01:55:17 +0900

    refactor: 리팩토링과 타입, 변수명 변경

--- src/screens/library/LibraryScreen.tsx
+++ src/screens/library/LibraryScreen.tsx
@@ -3,31 +3,33 @@ import Header from '../../components/header/Header';
 import {useEffect, useState} from 'react';
 import LibraryTimer from '../../components/molecules/library/LibraryTimer';
 import {Txt} from '@uoslife/design-system';
-import {CardLayout} from '../../components/molecules';
+import LibraryCardItem from '../../components/molecules/library/LibraryCardItem';
 
 type LibraryUserInfo =
   | {
       userName: string;
-      state: '이용 중' | '외출 중'; // 사용자 상태
-      timerTime: number; // 타이머에 표시: 초 단위로
+      using: true; // 사용여부
+      leave: boolean; // 외출여부
+      timerTime: number; // 타이머에 표시될 시간: 초 단위
       room: string; // 열람실: 가능하면 type을 나중에 더 specific하게 변경하기
       seatNum: number; // 좌석번호
       timeOfUse: string; // 이용시간: 가능하면 type을 나중에 더 specific하게 변경하기
       extended: number; // 연장 횟수: 0 - 3회
     }
   | {
-      state: '비이용'; // 사용자 상태
+      using: false;
     };
 
 // Header 상단바에 겹침 이슈 해결 필요
 const LibraryScreen = () => {
   const [libraryUserInfo, setLibraryUserInfo] = useState<LibraryUserInfo>({
     userName: '한유민',
-    state: '이용 중',
+    using: true,
+    leave: false,
     timerTime: 1000,
-    room: '쏼라쏼라',
+    room: '쏼라쏼라열람실',
     seatNum: 21,
-    timeOfUse: '30:00 - 90:00',
+    timeOfUse: '14:00 - 20:00',
     extended: 0,
   });
 
@@ -35,63 +37,69 @@ const LibraryScreen = () => {
   useEffect(() => {}, []);
 
   const UserInfo = () => {
-    switch (libraryUserInfo.state) {
-      case '비이용':
-        return (
-          <S.userInfoWrapper>
-            <S.timerContainer>
-              <LibraryTimer state={libraryUserInfo.state} />
-            </S.timerContainer>
-          </S.userInfoWrapper>
-        );
-      default:
-        const {room, seatNum, timeOfUse, extended} = libraryUserInfo;
+    const usingStatus = !libraryUserInfo.using
+      ? 3 // 3: 미사용
+      : !libraryUserInfo.leave
+      ? 0 // 0: 사용, 도서관 내부
+      : libraryUserInfo.timerTime > 1800
+      ? 1 // 1: 사용, 외출, 30분 초과 남음
+      : 2; // 2: 사용, 외출, 30분 이하 남음
 
-        // 남은 시간에 따라 달라지게 해야 함: 아직 미구현
-        const processedSaying = `${libraryUserInfo.userName}님 잘하고 있어요!`;
+    if (!libraryUserInfo.using)
+      return (
+        <S.userInfoWrapper>
+          <S.timerContainer>
+            <LibraryTimer usingStatus={usingStatus} />
+          </S.timerContainer>
+        </S.userInfoWrapper>
+      );
 
-        const processedDescriptionArray = [
-          ['열람실', room],
-          ['좌석번호', `${seatNum}번`],
-          ['이용시간', timeOfUse],
-          ['연장횟수', `${extended}회 / 3회`],
-        ];
+    const {room, seatNum, timeOfUse, extended, userName} = libraryUserInfo;
 
-        return (
-          <S.userInfoWrapper>
-            <S.sayingContainer>
+    const sayingArray = [
+      '잘하고 있어요!',
+      '조심히 다녀오세요!',
+      '시간이 얼마 남지 않았어요!',
+    ];
+    const processedSaying = `${userName}님 ` + sayingArray[usingStatus];
+
+    const processedDescriptionArray = [
+      ['열람실', room],
+      ['좌석번호', `${seatNum}번`],
+      ['이용시간', timeOfUse],
+      ['연장횟수', `${extended}회 / 3회`],
+    ];
+
+    return (
+      <S.userInfoWrapper>
+        <S.sayingContainer>
+          <Txt
+            color={'grey190'}
+            label={processedSaying}
+            typograph={'titleLarge'}
+          />
+        </S.sayingContainer>
+        <S.timerContainer>
+          <LibraryTimer
+            usingStatus={usingStatus}
+            timerTime={libraryUserInfo.timerTime}
+          />
+        </S.timerContainer>
+        <S.userInfoDetails>
+          {processedDescriptionArray.map((item, i) => (
+            <S.detailItem key={i}>
               <Txt
-                color={'grey190'}
-                label={processedSaying}
-                typograph={'titleLarge'}
+                style={{width: 56}}
+                color={'grey90'}
+                label={item[0]}
+                typograph="titleSmall"
               />
-            </S.sayingContainer>
-            <S.timerContainer>
-              <LibraryTimer
-                state={libraryUserInfo.state}
-                timerTime={libraryUserInfo.timerTime}
-              />
-            </S.timerContainer>
-            <S.userInfoDescription>
-              {processedDescriptionArray.map((item, i) => (
-                <S.descriptionItemContainer key={i}>
-                  <Txt
-                    style={{width: 56}}
-                    color={'grey90'}
-                    label={item[0]}
-                    typograph="titleSmall"
-                  />
-                  <Txt
-                    color={'grey190'}
-                    label={item[1]}
-                    typograph="bodyLarge"
-                  />
-                </S.descriptionItemContainer>
-              ))}
-            </S.userInfoDescription>
-          </S.userInfoWrapper>
-        );
-    }
+              <Txt color={'grey190'} label={item[1]} typograph="bodyLarge" />
+            </S.detailItem>
+          ))}
+        </S.userInfoDetails>
+      </S.userInfoWrapper>
+    );
   };
 
   const SeatStatus = () => (
@@ -99,7 +107,7 @@ const LibraryScreen = () => {
       <S.seatStatusTopTextContainer>
         <Txt color="grey190" label="도서관 좌석 현황" typograph="titleLarge" />
       </S.seatStatusTopTextContainer>
-      {/* Card */}
+      <LibraryCardItem />
     </S.seatStatusWrapper>
   );
 
@@ -137,7 +145,7 @@ const S = {
   timerContainer: styled.View`
     padding-top: 24px;
   `,
-  userInfoDescription: styled.View`
+  userInfoDetails: styled.View`
     display: flex;
     gap: 8px;
 
@@ -146,7 +154,7 @@ const S = {
     padding-top: 24px;
     padding-left: 48px;
   `,
-  descriptionItemContainer: styled.View`
+  detailItem: styled.View`
     display: flex;
     flex-direction: row;
     gap: 24px;
