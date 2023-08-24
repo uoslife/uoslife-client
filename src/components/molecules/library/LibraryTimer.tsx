601cccf0b33584b985af8ad33371a0db289a5e6f
limeojin363 <2002eojin@gmail.com>
2023-08-24 18:42:25 +0900

    feat: 기초적인 레이아웃 구현

---
+++ src/components/molecules/library/LibraryTimer.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const LibraryTimer = () => {
+  return (
+    <View>
+      <Text>LibraryTimer</Text>
+    </View>
+  );
+};
+
+export default LibraryTimer;
