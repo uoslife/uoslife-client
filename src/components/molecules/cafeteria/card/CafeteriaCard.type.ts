eb9205655d9589298d0c85349140a3bb795deb80
Song Chaehee <sch003322@gmail.com>
2023-08-26 13:23:56 +0900

    feat: CafeteriaCard 컴포넌트 기능, 스타일 구현

---
+++ src/components/molecules/cafeteria/card/CafeteriaCard.type.ts
@@ -0,0 +1,9 @@
+import { CafeteriaItem } from "../../../../screens/cafeteria/CafeteriaScreen";
+
+export type CafeteriaCardProps = {
+    cafeteriaItems:CafeteriaItem[];
+    isEmpty?:boolean;
+}
+  
+export default CafeteriaCardProps;
+  
