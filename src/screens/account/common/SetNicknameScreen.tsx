6b57bb257f2cbad70a2746c04eae1df7e8b88760
youngchan Kim <aacz12035@gmail.com>
2023-08-26 11:03:29 +0900

    [App-50] Account 페이지 최종 디자인 제작 및 리팩터링 (#45)
    ### Account 페이지 최종 디자인 제작 및 리팩터링
    
    ## 개요
    - 완성된 컴포넌트(input, button 등)을 갖고 와이어프레임으로만 작업했던 모든 Account 페이지들을 완성된 디자인에
    맞게 제작했습니다.
    
    ### 세부설명
    제작한 페이지들은 이렇습니다.
    -휴대폰 인증
    <img width="1009" alt="image"
    src="https://github.com/uoslife/rebuild-client/assets/89263598/955526ee-9837-4cfd-8b3f-0a7c2e905356">
    
    -신규유저
    <img width="1026" alt="image"
    src="https://github.com/uoslife/rebuild-client/assets/89263598/9da8fec4-1b55-4a7e-bc4e-94f7c160bee1">
    
    -기존유저
    <img width="1109" alt="image"
    src="https://github.com/uoslife/rebuild-client/assets/89263598/9736f6cd-947e-48de-b70e-5d51cb5d126b">
    
    -포탈 인증
    <img width="348" alt="image"
    src="https://github.com/uoslife/rebuild-client/assets/89263598/363cca8a-2b14-4ec9-a2fb-0ce074b29da6">
    
    ## 개발하면서 어려웠던 점
    - 최대한 기존의 코드를 보존하고자 노력은 하였으나 와이어프레임 단계의 코드를 재활용하기가 어려워, 처음부터 다시 제작한
    페이지들이 많습니다. 본인의 코드가 바뀐 것에 아쉬움 마음이 들 수 있으시겠지만 양해 부탁드리겠습니다.

--- src/screens/account/common/SetNicknameScreen.tsx
+++ src/screens/account/common/SetNicknameScreen.tsx
@@ -1,51 +1,22 @@
 import styled from '@emotion/native';
-import React, {Dispatch, SetStateAction, useState} from 'react';
-import {
-  View,
-  NativeSyntheticEvent,
-  TextInputChangeEventData,
-  Image,
-} from 'react-native';
-import {TextInput} from 'react-native-gesture-handler';
-import {Button} from '../../../components/button/Button';
+import React, {useEffect, useState} from 'react';
+import {View} from 'react-native';
 import AgreementToTerms from '../../../components/contents/AgreementToTerms';
 import AgreementProcessResult from '../../../components/contents/AgreementProcessResult';
+import BottomSheet from '../../../components/modals/BottomSheet';
+import Modal from '../../../components/modals/Modal';
 import {useSetAtom} from 'jotai';
 import {accountStatusAtom} from '..';
-
-interface InputProps {
-  text: string;
-  isChecked: boolean;
-}
+import {Button, Txt} from '@uoslife/design-system';
+import Header from '../../../components/header/Header';
+import Input from '../../../components/forms/input/Input';
 
 const SetNicknameScreen = () => {
-  const [input, setInput] = useState<InputProps>({
-    text: '',
-    isChecked: false,
-  });
+  const [inputValue, setInputValue] = useState('');
   const [isBottomSheetOpened, setIsBottomSheetOpened] =
     useState<boolean>(false);
   const [isModalOpened, setIsModalOpened] = useState<boolean>(false);
-  const [errorMessage, setErrorMessage] = useState<string>('');
-
-  const onChange = (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
-    const text = e.nativeEvent.text;
-
-    const regex = /^[가-힣a-zA-Z0-9!@#$%^&*()-_+=~]{1,8}$/;
-
-    const isValid = regex.test(text); // 정규표현식 일치 여부 확인
-    let isVerified = false; // 중복되지 않았는지 확인: API 붙인 뒤 로직 작성
-
-    try {
-      isVerified = true;
-    } catch (err) {
-      console.log(err);
-    }
-
-    const isChecked = isValid && isVerified;
-
-    setInput({text, isChecked});
-  };
+  const [statusMessage, setStatusMessage] = useState<string>('');
 
   const openModal = () => {
     setIsModalOpened(true);
@@ -71,44 +42,64 @@ const SetNicknameScreen = () => {
       };
     });
   };
+
+  const handleStatusMessage = (status: string) => {
+    switch (status) {
+      case 'codeError':
+        return '사용할 수 없는 닉네임입니다.';
+      case 'successNickname':
+        return '사용 가능한 닉네임입니다.';
+      case 'duplicateedNickname':
+        return '중복되는 닉네임입니다.';
+      default:
+        return '';
+    }
+  };
+
+  useEffect(() => {
+    // 기존 유저일 시, input의 status 변경 로직 추가.
+  }, []);
+
   return (
     <>
-      <S.pageWrapper>
-        <S.contentsContainer>
-          <S.description1>사용하실 닉네임을 입력해주세요.</S.description1>
-          <View>
-            <S.description2>
-              닉네임은 최대 8자로 설정 가능합니다.
-            </S.description2>
-            <S.description2>
-              한글, 영문, 숫자, 특수기호를 이용해주세요.
-            </S.description2>
-          </View>
-          <View>
-            <View>
-              <S.literalNickname>닉네임</S.literalNickname>
-            </View>
-            <S.nicknameAndCheck>
-              <TextInput
-                value={input.text}
-                onChange={onChange}
-                style={{
-                  borderColor: 'black',
-                  borderStyle: 'solid',
-                  borderWidth: 5,
-                  flex: 1,
-                }}
+      <S.screenContainer>
+        <Header label={'닉네임 설정'} />
+        <S.setNicknameContainer>
+          <View style={{gap: 32}}>
+            <View style={{gap: 8}}>
+              <Txt
+                label={'사용하실 닉네임을 입력해주세요.'}
+                color={'grey190'}
+                typograph={'headlineMedium'}
               />
-            </S.nicknameAndCheck>
+              <Txt
+                label={
+                  '닉네임은 최대 8자로 설정 가능합니다.\n한글, 영문, 숫자, 특수기호를 이용해주세요.'
+                }
+                color={'grey190'}
+                typograph={'bodyMedium'}
+              />
+            </View>
+            <Input
+              onChangeText={text => setInputValue(text)}
+              maxLength={8}
+              onPress={() => setInputValue('')}
+              value={inputValue}
+              label={'닉네임'}
+              statusMessage={handleStatusMessage(statusMessage)}
+              status={'default'}
+              placeholder={'여기에 입력하세요.'}
+            />
           </View>
-        </S.contentsContainer>
-        <Button
-          label={'확인'}
-          onPress={openBottomSheet}
-          type={input.isChecked ? 'primary' : 'default'}
-        />
-      </S.pageWrapper>
-      {/* {isBottomSheetOpened && (
+          <Button
+            label={'설정하기'}
+            onPress={openBottomSheet}
+            isEnabled={!!inputValue}
+            isFullWidth={true}
+          />
+        </S.setNicknameContainer>
+      </S.screenContainer>
+      {isBottomSheetOpened && (
         <BottomSheet
           zIndex={1}
           onBackgroundPress={closeBottomSheet}
@@ -123,7 +114,7 @@ const SetNicknameScreen = () => {
         <Modal onClose={closeModal} zIndex={3} darkBackground>
           <AgreementProcessResult />
         </Modal>
-      )} */}
+      )}
     </>
   );
 };
@@ -131,44 +122,12 @@ const SetNicknameScreen = () => {
 export default SetNicknameScreen;
 
 const S = {
-  pageWrapper: styled.View`
+  screenContainer: styled.View`
     flex: 1;
-    display: flex;
-    flex-direction: column;
-
-    padding-top: 28px;
-    padding-right: 16px;
-    padding-left: 16px;
-    padding-bottom: 28px;
+  `,
+  setNicknameContainer: styled.View`
+    flex: 1;
     justify-content: space-between;
-  `,
-  nicknameAndCheck: styled.View`
-    display: flex;
-    flex-direction: row;
-    gap: 4px;
-  `,
-  contentsContainer: styled.View`
-    display: flex;
-    flex-direction: column;
-    gap: 32px;
-  `,
-  description1: styled.Text`
-    font-size: 25px;
-    font-weight: bold;
-    color: black;
-  `,
-  description2: styled.Text`
-    font-size: 18px;
-    color: black;
-  `,
-  inputContainer: styled.View`
-    display: flex;
-    flex-direction: column;
-    gap: 28px;
-  `,
-  literalNickname: styled.Text`
-    padding-left: 12px;
-    padding-right: 12px;
-    color: black;
+    padding: 28px 16px;
   `,
 };
