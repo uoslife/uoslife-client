e2f0c15f2cd4ea34fa9c723e034d5f486306bfe2
song_chaehee <109050721+Chaehee03@users.noreply.github.com>
2023-08-29 01:51:35 +0900

    [APP-88] AnnouncementStackNavigator 구조 수정 (#65)
    ### Summary
    
    - 공지사항의 AnnouncementStackNavigator 구조를 수정하였습니다.
    - 기존의 중첩된 폴더 구조를 삭제하고 파일로 바꿔 구조를 간략화했습니다. 즉,
    announcement/AnnouncementMainScreenContainer/index.tsx 구조에서,
    announcement/AnnouncementScreen.tsx 구조로 변경하였습니다.
    - step 과 관련된 파일을 삭제하였습니다. step이 아닌 stackNavigator로 네비게이션이 작동합니다.

--- src/navigators/AnnouncementStackNavigator.tsx
+++ src/navigators/AnnouncementStackNavigator.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-
-import AnnouncementTempScreen from '../screens/announcement/AnnouncementTempScreen';
+import AnnouncementMainScreen from '../screens/announcement/AnnouncementMainScreen';
+import AnnouncementDetailScreen from '../screens/announcement/AnnouncementDetailScreen';
+import AnnouncementBookmarkScreen from '../screens/announcement/AnnouncementBookmarkScreen';
+import AnnouncementSearchScreen from '../screens/announcement/AnnouncementSearchScreen';
 
 export type AnnouncementStackParamList = {
-  Announcement: undefined;
+  Announcement_main: undefined;
+  Announcement_detail: undefined;
+  Announcement_bookmark: undefined;
+  Announcement_search: undefined;
 };
 
 const Stack = createStackNavigator<AnnouncementStackParamList>();
@@ -12,9 +17,12 @@ const Stack = createStackNavigator<AnnouncementStackParamList>();
 const AnnouncementStackNavigator = () => {
   return (
     <Stack.Navigator
-      initialRouteName="Announcement"
-      screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Announcement" component={AnnouncementTempScreen} />
+    initialRouteName="Announcement_main"
+    screenOptions={{headerShown: false}}>
+    <Stack.Screen name="Announcement_main" component={AnnouncementMainScreen} />
+    <Stack.Screen name="Announcement_detail" component={AnnouncementDetailScreen} />
+    <Stack.Screen name="Announcement_bookmark" component={AnnouncementBookmarkScreen} />
+    <Stack.Screen name="Announcement_search" component={AnnouncementSearchScreen} />
     </Stack.Navigator>
   );
 };
---
+++ src/screens/announcement/AnnouncementSearchScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const AnnouncementSearchScreen = () => {
+  return (
+    <View><Text>AnnouncementSearchScreen</Text></View>
+  )
+}
+
+export default AnnouncementSearchScreen
