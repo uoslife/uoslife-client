87f38080dfc6d6117b4f6c37f71b145a7449efcb
Song Chaehee <sch003322@gmail.com>
2023-08-29 16:23:27 +0900

    fix: width 제거

--- src/components/molecules/termsOfService/text/MainTitle.tsx
+++ src/components/molecules/termsOfService/text/MainTitle.tsx
@@ -15,7 +15,6 @@ const MainTitle = ({mainTitle}: MianTitleProp) => {
 };
 
 const TitleContainer = styled.View`
-  width: 100%;
   padding: 20px 0px 28px 0px;
   align-items: center;
   flex-shrink: 0;
