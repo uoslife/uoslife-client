76cd60d42ceb98a772e9db17c4df39fb36ecc767
Song Chaehee <sch003322@gmail.com>
2023-08-29 11:56:13 +0900

    feat: MainTitle 스타일 구현

---
+++ src/components/molecules/termsOfService/text/MainTitle.tsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/native';
+import {Txt} from '@uoslife/design-system';
+import React from 'react';
+
+type MianTitleProp = {
+  mainTitle: string;
+};
+
+const MainTitle = ({mainTitle}: MianTitleProp) => {
+  return (
+    <TitleContainer>
+      <Txt label={mainTitle} color={'grey190'} typograph={'headlineMedium'} />
+    </TitleContainer>
+  );
+};
+
+const TitleContainer = styled.View`
+  width: 328px;
+  padding: 20px 0px 28px 0px;
+  align-items: center;
+  flex-shrink: 0;
+`;
+
+export default MainTitle;
