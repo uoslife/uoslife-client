b657e9547c387b2f2232ef60bbe4c49a9bf4dd04
Song Chaehee <sch003322@gmail.com>
2023-08-30 16:14:06 +0900

    feat: Paragraph 컴포넌트 padding 수정

--- src/components/molecules/termsOfService/text/Paragraph.tsx
+++ src/components/molecules/termsOfService/text/Paragraph.tsx
@@ -12,32 +12,30 @@ type ParagraphProps = {
 const Paragraph = ({header, subHeader, paragraph}: ParagraphProps) => {
   return (
     <View>
-      <S.headerContainer>
-        {header && (
+      {header && (
+        <S.headerContainer>
           <Txt label={header} color={'grey190'} typograph={'titleLarge'} />
-        )}
-      </S.headerContainer>
-      <S.subHeaderContainer>
-        {subHeader && (
+        </S.headerContainer>
+      )}
+      {subHeader && (
+        <S.subHeaderContainer>
           <Txt label={subHeader} color={'grey190'} typograph={'titleSmall'} />
-        )}
-      </S.subHeaderContainer>
-      <View>
-        {paragraph && (
-          <Txt label={paragraph} color={'grey130'} typograph={'bodyLarge'} />
-        )}
-      </View>
+        </S.subHeaderContainer>
+      )}
+      {paragraph && (
+        <Txt label={paragraph} color={'grey130'} typograph={'bodyLarge'} />
+      )}
     </View>
   );
 };
 
 const S = {
   headerContainer: styled.View`
-    padding: 16px 0px;
+    padding: 16px 0px 16px 0px;
   `,
   subHeaderContainer: styled.View`
-    padding: 0px 0px 12px 0px,
-  `
+    padding: 0px 0px 12px 0px;
+  `,
 };
 
 export default Paragraph;
