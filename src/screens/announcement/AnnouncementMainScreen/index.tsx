55ed0d95bd509b65e0fbefbcc84af678b28a8cc7
Limeojin <2002eojin@gmail.com>
2023-09-02 15:32:40 +0900

    [APP-77] 공지사항 페이지에 Navigation, 확정된 디자인 적용 (#76)
    # Summary
    - SPA로 땜빵했던 페이지 이동에, react-navigation을 적용하여 제대로 작동하도록 하였습니다.
    - 확정된 디자인을 (대부분) 적용하였습니다.
    - SearchInput을 일부 수정하였습니다.
    
    # Details
    ## SearchWindow <-> SearchResult - 개별 페이지 스택 VS SPA
    - 검색창과 검색결과 두 상태를 페이지를 분리할까도 생각했지만, Reference를 좀 찾아보다가 일단 페이지를 분리하지 않고
    SPA로 구현하기로 결정하였습니다. UI/UX 측면에서는 각 장단점이 있고, DX 측면에서는 후자가 나은 것 같습니다.
    - 개별 페이지 스택: 유튜브, 위키피디아(모바일 앱) 등
    - SPA: 쿠팡, 에브리타임, 시대생(기존 앱) 등
    
    # To-do
    - Header에 아이콘, SearchInput을 정상적으로 주입할 수 있도록 수정 후 디자인 반영이 필요합니다.
    - SearchInput에서 이외 영역 터치시 focus가 사라져야 할 때 사라지지 않는 이슈가 있어 해결이 필요합니다.
    - API 작업이 필요합니다.

---
+++ src/screens/announcement/AnnouncementMainScreen/index.tsx
@@ -0,0 +1,133 @@
+import React, {useState, useEffect} from 'react';
+import {View, Pressable} from 'react-native';
+import Header from '../../../components/header/Header';
+import styled from '@emotion/native';
+import ArticleList from '../../../components/molecules/announcement/article/ArticleList';
+import CategoryTab from '../../../components/category-tab/CategoryTab';
+import {Icon, Txt} from '@uoslife/design-system';
+import {AnnouncementNavigationProps} from '../../../navigators/AnnouncementStackNavigator';
+import {useNavigation} from '@react-navigation/native';
+
+export type ArticleCategoryName =
+  | '일반공지'
+  | '학사공지'
+  | '채용공고'
+  | '창업공지';
+export type ArticleCategoryTapState = {
+  list: ArticleCategoryName[];
+  selected: ArticleCategoryName;
+};
+export type Article = {
+  bookmarkCnt: number;
+  bookmarkByMe: boolean;
+  title: string;
+  category: ArticleCategoryName;
+  body: string;
+  department: string; // XX과
+  uploadTime: Date;
+  id: string;
+  attachments: string[]; // 첨부파일
+};
+
+export const ANNOUNCEMENT_ARTICLE_DUMMY_DATA: Article[] = new Array(15)
+  .fill(null)
+  .map((_, i) => ({
+    bookmarkCnt: i % 5,
+    department: `category${i}`,
+    title: `titletitletitletitletitletitletitletitletitletitleletitletitletitle${i}`,
+    uploadTime: new Date(),
+    bookmarkByMe: !!(i % 5) && !!(i % 2),
+    body: 'bodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybody',
+    id: `id${i}`,
+    category:
+      i % 4 === 0
+        ? '일반공지'
+        : i % 4 === 1
+        ? '학사공지'
+        : i % 4 === 2
+        ? '채용공고'
+        : '창업공지',
+    attachments: i % 3 === 0 ? [] : ['첨부파일 1', '첨부파일 2'],
+  }));
+
+const AnnouncementMainScreen = () => {
+  const [articles, setArticles] = useState<Article[]>([]);
+  const [articleCategoryTapProps, setArticleCategoryTapProps] =
+    useState<ArticleCategoryTapState>({
+      list: ['일반공지', '학사공지', '채용공고', '창업공지'],
+      selected: '일반공지',
+    });
+
+  const selectCategory = (categoryName: ArticleCategoryName) => {
+    setArticleCategoryTapProps({
+      ...articleCategoryTapProps,
+      selected: categoryName,
+    });
+  };
+
+  const navigation = useNavigation<AnnouncementNavigationProps>();
+
+  useEffect(() => {
+    try {
+      // 선택한 메뉴에 해당되는 글만
+      setArticles(
+        ANNOUNCEMENT_ARTICLE_DUMMY_DATA.filter(
+          article => article.category === articleCategoryTapProps.selected,
+        ),
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  }, [articleCategoryTapProps]);
+
+  return (
+    <S.screenWrapper>
+      <Header label="공지사항" />
+      {/* 헤더 완성시 검색, 북마크, 알림 아이콘 넣기 */}
+      <View>
+        <Pressable
+          onPress={() => {
+            navigation.navigate('AnnouncementBookmark');
+          }}>
+          <Icon name={'bookmark'} color={'grey150'} height={32} width={32} />
+        </Pressable>
+        <Pressable
+          onPress={() => {
+            navigation.navigate('AnnouncementSearch');
+          }}>
+          <Icon name={'search'} color={'grey150'} height={32} width={32} />
+        </Pressable>
+        <Pressable>
+          <Icon
+            name={'notification'}
+            color={'grey150'}
+            height={32}
+            width={32}
+          />
+        </Pressable>
+      </View>
+      <S.categoryTapAndContents>
+        <CategoryTab
+          categoryTabProps={articleCategoryTapProps}
+          selectCategory={selectCategory}
+        />
+        <ArticleList articles={articles} />
+      </S.categoryTapAndContents>
+    </S.screenWrapper>
+  );
+};
+
+export default AnnouncementMainScreen;
+
+const S = {
+  screenWrapper: styled.ScrollView`
+    width: 100%;
+    height: 100%;
+    display: flex;
+  `,
+  categoryTapAndContents: styled.View`
+    width: 100%;
+    display: flex;
+    gap: 4px;
+  `,
+};
