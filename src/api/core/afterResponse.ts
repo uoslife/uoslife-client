03e832b748b2e427c4dbad0bbfd080f7d2057d19
은배 <geb9211@gmail.com>
2023-09-05 22:34:41 +0900

    feat: api response에 status 코드를 덮어씌우도록 변경

--- src/api/core/afterResponse.ts
+++ src/api/core/afterResponse.ts
@@ -2,20 +2,15 @@ import ky, {AfterResponseHook} from 'ky';
 import {CoreAPI} from '../services';
 import setTokenWhenLogin from '../../utils/setTokenWhenLogin';
 
-const isDev = process.env.NODE_ENV !== 'production';
-
-export const logging: AfterResponseHook = (request, _options, response) => {
-  if (isDev) console.log(response);
-  return ky(request);
-};
-
 export const handleToken: AfterResponseHook = async (
   request,
   _options,
   response,
 ) => {
-  console.info(await response.json());
-  if (response.status !== 401) return ky(request);
+  const jsonRes = await response.json();
+  const blob = new Blob([JSON.stringify(jsonRes, null, 2)]);
+  if (response.status !== 401)
+    return new Response(blob, {status: jsonRes.status});
   const res = await CoreAPI.getRefreshToken({});
   if (res.statusCode === 201) {
     setTokenWhenLogin(res.accessToken, res.refreshToken);
