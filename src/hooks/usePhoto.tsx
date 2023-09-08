1c82d849e7446f4dd11c152140e4f4bbf476e3df
youngchan Kim <aacz12035@gmail.com>
2023-09-09 02:27:46 +0900

    refactor: usePhoto 훅 기능별 구조화

--- src/hooks/usePhoto.tsx
+++ src/hooks/usePhoto.tsx
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import {Alert} from 'react-native';
+import {Alert, AlertButton} from 'react-native';
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 type PhotoHookReturnType = [string, () => void];
@@ -7,50 +7,35 @@ type PhotoHookReturnType = [string, () => void];
 const usePhoto = (initialValue: string): PhotoHookReturnType => {
   const [photoValue, setPhotoValue] = useState(initialValue);
 
-  const imagePickerType = {
-    launchImageLibrary: {
-      text: '사진 보관함',
-      onPress: async () => {
-        try {
-          const result = await launchImageLibrary({
-            mediaType: 'photo',
-          });
-          if (result.didCancel) return null;
-          setPhotoValue(result.assets![0].uri ?? '');
-        } catch (e) {
-          console.error(e);
-        }
-      },
-    },
-    launchCamera: {
-      text: '사진 찍기',
-      onPress: async () => {
-        try {
-          const result = await launchCamera({
-            mediaType: 'photo',
-            cameraType: 'back',
-          });
-          if (result.didCancel) return null;
-          setPhotoValue(result.assets![0].uri ?? '');
-        } catch (e) {
-          console.error(e);
-        }
-      },
-    },
+  const selectImageFromLibrary = async () => {
+    try {
+      const result = await launchImageLibrary({mediaType: 'photo'});
+      if (!result.didCancel) setPhotoValue(result.assets![0].uri ?? '');
+    } catch (e) {
+      console.error(e);
+    }
   };
 
-  const selectPhoto = () => {
-    Alert.alert(
-      '사진 변경',
-      '',
-      [
-        {text: '취소', style: 'cancel'},
-        imagePickerType.launchImageLibrary,
-        imagePickerType.launchCamera,
-      ],
-      {cancelable: true},
-    );
+  const takePhotoWithCamera = async () => {
+    try {
+      const result = await launchCamera({
+        mediaType: 'photo',
+        cameraType: 'back',
+      });
+      if (!result.didCancel) setPhotoValue(result.assets![0].uri ?? '');
+    } catch (e) {
+      console.error(e);
+    }
   };
+
+  const selectPhotoOptions: AlertButton[] = [
+    {text: '취소', style: 'cancel'},
+    {text: '사진 보관함', onPress: selectImageFromLibrary},
+    {text: '사진 찍기', onPress: takePhotoWithCamera},
+  ];
+
+  const selectPhoto = () =>
+    Alert.alert('사진 변경', '', selectPhotoOptions, {cancelable: true});
 
   return [photoValue, selectPhoto];
 };
