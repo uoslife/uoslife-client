286145f4ab3fc91b1feafe4ad90684da3bb62ccf
youngchan Kim <aacz12035@gmail.com>
2023-09-13 15:12:54 +0900

    feat: 시대팅 페이지 제작

--- src/screens/UoslifeMeetingScreen.tsx
+++ src/screens/UoslifeMeetingScreen.tsx
@@ -1,9 +1,20 @@
 import styled from '@emotion/native';
-import {Text} from 'react-native';
+import {Txt} from '@uoslife/design-system';
 const UoslifeMeetingScreen = () => {
   return (
     <S.screenContainer>
-      <Text>uoslife-meeting</Text>
+      <S.uoslifeBrandLogo
+        source={require('../assets/images/uoslifeBrand작Logo.png')}
+      />
+      <S.titleContainer>
+        <S.confettiEmoticon>🎉</S.confettiEmoticon>
+        <Txt
+          label={'시대팅 준비중에 있어요'}
+          color={'grey190'}
+          typograph={'headlineLarge'}
+        />
+        <S.confettiEmoticon>🎉</S.confettiEmoticon>
+      </S.titleContainer>
     </S.screenContainer>
   );
 };
@@ -11,7 +22,23 @@ const UoslifeMeetingScreen = () => {
 export default UoslifeMeetingScreen;
 
 const S = {
-  screenContainer: styled.ScrollView`
+  screenContainer: styled.View`
     flex: 1;
+    justify-content: center;
+    align-items: center;
+    gap: 30px;
+  `,
+  uoslifeBrandLogo: styled.Image`
+    width: 85%;
+    height: 20%;
+  `,
+  titleContainer: styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 7px;
+  `,
+  confettiEmoticon: styled.Text`
+    font-size: 25px;
   `,
 };
