51fb97e8c753d8193e35be3dbcb1e91f94a7281f
은배 <geb9211@gmail.com>
2023-09-17 16:42:44 +0900

    feat: token 관리 로직 변경

--- src/api/core/beforeRequest.ts
+++ src/api/core/beforeRequest.ts
@@ -1,13 +1,21 @@
 import ky, {BeforeRequestHook} from 'ky';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const getData = async (key: string) => {
+export const getData = async (key: string) => {
   try {
     const value = await AsyncStorage.getItem(key);
     if (value !== null) {
       const data = JSON.parse(value);
       return data;
     }
+  } catch (e: any) {
+    console.log(e.message);
+  }
+};
+
+export const removeData = async (key: string) => {
+  try {
+    await AsyncStorage.removeItem(key);
   } catch (e: any) {
     console.log(e.message);
   }
