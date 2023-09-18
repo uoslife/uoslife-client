1d44859d1ae1f03e96242813cda0c3161bfa5d1d
youngchan Kim <aacz12035@gmail.com>
2023-09-18 20:59:37 +0900

    design: input 컴포넌트 border-bottom width 변경

--- src/components/forms/input/Input.tsx
+++ src/components/forms/input/Input.tsx
@@ -79,7 +79,7 @@ const S = {
     justify-content: space-between;
     width: 100%;
     padding: 5px 12px 8px 12px;
-    border-bottom-width: ${({value}) => (!!value ? '2px' : '1px')};
+    border-bottom-width: 1px;
     border-bottom-color: ${({status}) => getStatusColor(status!)};
   `,
   textInput: styled.TextInput<InputProps>`
