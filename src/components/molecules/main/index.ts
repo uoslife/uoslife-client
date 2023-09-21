199ea73e597c0a5e2b96b52f797f1dd6c706cb42
은배 <geb9211@gmail.com>
2023-09-21 23:50:49 +0900

    feat: banner 컴포넌트 삭제

--- src/components/molecules/main/index.ts
+++ src/components/molecules/main/index.ts
@@ -1,4 +1,3 @@
-export {default as Banner} from './banner/Banner';
 export {default as CardLayout} from './cardLayout/CardLayout';
 export {default as MainServiceBox} from './mainServiceBox/MainServiceBox';
 export {default as BottomNavigation} from './bottomNavigation/BottomNavigation';
