7ff66357b57f488fb4964eec4c964364ffc39dad
limeojin363 <2002eojin@gmail.com>
2023-09-25 15:58:49 +0900

    refactor: 컴포넌트 분리와 가독성 개선

--- src/screens/announcement/AnnouncementBookmarkBoxScreen.tsx
+++ src/screens/announcement/AnnouncementBookmarkBoxScreen.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import {View} from 'react-native';
 import Header from '../../components/header/Header';
 import {Txt} from '@uoslife/design-system';
 import ArticleList from '../../components/molecules/announcement/article/ArticleList';
@@ -7,19 +6,23 @@ import styled from '@emotion/native';
 import {
   ANNOUNCEMENT_ARTICLE_DUMMY_DATA,
   Article,
-  ArticleCategoryTapState,
 } from './AnnouncementMainScreen';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {useNavigation} from '@react-navigation/native';
 
+const NoBookmarkFound = () => (
+  <S.NoBookmarkFoundContainer>
+    <Txt
+      color={'black'}
+      label={'자신이 북마크한 공지사항을 확인할 수 있어요'}
+      typograph={'bodyMedium'}
+    />
+  </S.NoBookmarkFoundContainer>
+);
+
 const AnnouncementBookmarkBoxScreen = () => {
   const insets = useSafeAreaInsets();
   const [articles, setArticles] = useState<Article[]>([]);
-  useState<ArticleCategoryTapState>({
-    list: ['일반공지', '학사공지', '채용공고', '창업공지'],
-    selected: '일반공지',
-  });
-
   const navigation = useNavigation();
   const handleGoBack = () => {
     navigation.goBack();
@@ -29,7 +32,9 @@ const AnnouncementBookmarkBoxScreen = () => {
     try {
       // 내가 북마크한 글만
       setArticles(
-        ANNOUNCEMENT_ARTICLE_DUMMY_DATA.filter(article => article.bookmarkByMe),
+        ANNOUNCEMENT_ARTICLE_DUMMY_DATA.filter(
+          ({bookmarkByMe}) => bookmarkByMe,
+        ),
       );
     } catch (err) {
       console.log(err);
@@ -37,39 +42,34 @@ const AnnouncementBookmarkBoxScreen = () => {
   }, []);
 
   return (
-    <S.screenWrapper style={{paddingTop: insets.top}}>
+    <S.ScreenContainer style={{paddingTop: insets.top}}>
       <Header label="북마크함" onPressBackButton={handleGoBack} />
-      <S.categoryTapAndContents>
+      <S.BookmarkListContainer>
         {articles.length === 0 ? (
-          <View style={{paddingTop: 48, display: 'flex', alignItems: 'center'}}>
-            <Txt
-              color={'black'}
-              label={'자신이 북마크한 공지사항을 확인할 수 있어요'}
-              typograph={'bodyMedium'}
-            />
-          </View>
+          <NoBookmarkFound />
         ) : (
           <ArticleList articles={articles} showCategory />
         )}
-      </S.categoryTapAndContents>
-    </S.screenWrapper>
+      </S.BookmarkListContainer>
+    </S.ScreenContainer>
   );
 };
 
 export default AnnouncementBookmarkBoxScreen;
+
 const S = {
-  screenWrapper: styled.ScrollView`
+  ScreenContainer: styled.ScrollView`
     width: 100%;
     height: 100%;
     display: flex;
   `,
-  categoryTapAndContents: styled.View`
+  BookmarkListContainer: styled.View`
     width: 100%;
     display: flex;
     gap: 4px;
   `,
-  decriptionContainer: styled.View`
-    display: flex;
-    flex-direction: row;
+  NoBookmarkFoundContainer: styled.View`
+    padding-top: 48px;
+    align-items: center;
   `,
 };
