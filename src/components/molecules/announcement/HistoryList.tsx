620f2258d61796679abfdae535286e2f725df33f
limeojin363 <2002eojin@gmail.com>
2023-09-26 16:54:01 +0900

    rename: 변수명 변경

--- src/components/molecules/announcement/HistoryList.tsx
+++ src/components/molecules/announcement/HistoryList.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from '@emotion/native';
 import {Icon, Txt} from '@uoslife/design-system';
-import {Alert, Pressable} from 'react-native';
+import {Alert} from 'react-native';
 
 const HistoryList = ({
   histories,
@@ -11,37 +11,37 @@ const HistoryList = ({
   executeSearch: (searchWord: string) => void;
 }) => {
   return (
-    <S.listContainer>
+    <S.HistoryListContainer>
       {histories.map(item => (
-        <S.historyItemContainer
+        <S.HistoryItemContainer
           onPress={() => {
             executeSearch(item);
           }}
           key={item}>
-          <S.iconAndTxt>
+          <S.ItemLeftBox>
             <Icon color={'grey130'} name={'history'} height={24} width={24} />
             <Txt label={item} color={'grey130'} typograph={'bodyLarge'} />
-          </S.iconAndTxt>
-          <Pressable
+          </S.ItemLeftBox>
+          <S.DeleteBtn
             onPress={e => {
               e.stopPropagation();
               Alert.alert('삭제');
             }}>
             <Icon name="clear" color="grey90" width={24} height={24} />
-          </Pressable>
-        </S.historyItemContainer>
+          </S.DeleteBtn>
+        </S.HistoryItemContainer>
       ))}
-    </S.listContainer>
+    </S.HistoryListContainer>
   );
 };
 
 export default HistoryList;
 
 const S = {
-  listContainer: styled.ScrollView`
+  HistoryListContainer: styled.ScrollView`
     padding: 0 16px;
   `,
-  historyItemContainer: styled.Pressable`
+  HistoryItemContainer: styled.Pressable`
     display: flex;
     flex-direction: row;
     align-items: center;
@@ -50,9 +50,10 @@ const S = {
     padding: 12px 4px 12px 8px;
     width: 100%;
   `,
-  iconAndTxt: styled.View`
+  ItemLeftBox: styled.View`
     display: flex;
     flex-direction: row;
     gap: 8px;
   `,
+  DeleteBtn: styled.Pressable``,
 };
