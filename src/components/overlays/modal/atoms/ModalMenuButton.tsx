459b47a31a17e9c19543d1e95e99894e9e159c5e
limeojin363 <2002eojin@gmail.com>
2023-09-30 02:53:23 +0900

    design: 알맞은 Icon으로 수정

--- src/components/overlays/modal/atoms/ModalMenuButton.tsx
+++ src/components/overlays/modal/atoms/ModalMenuButton.tsx
@@ -11,9 +11,7 @@ const ModalMenuButton = ({label, onPress}: ModalMenuButtonProps) => {
     <S.Wrapper>
       <S.Container onPress={onPress}>
         <Txt color={'grey190'} label={label} typograph={'bodyLarge'} />
-        {/* TODO: width, height 지정 제대로 했음에도 피그마에서보다 크게 보이는 문제 */}
-        {/* design-system에서 수정 필요 */}
-        <Icon name={'forward'} color={'grey130'} height={24} width={24} />
+        <Icon name={'forwardArrow'} color={'grey130'} height={24} width={24} />
       </S.Container>
     </S.Wrapper>
   );
@@ -22,7 +20,7 @@ const ModalMenuButton = ({label, onPress}: ModalMenuButtonProps) => {
 export default ModalMenuButton;
 
 const S = {
-  // Figma 페이지와 매칭을 위해 Wrapper 컴포넌트를 추가로 작성
+  // Figma 페이지와 매칭을 위해 padding을 부여하는 Wrapper 컴포넌트를 추가로 작성
   Wrapper: styled.Pressable`
     width: 100%;
     padding: 0px 16px;
