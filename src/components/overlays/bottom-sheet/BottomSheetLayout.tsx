15ff2b3ac14fce97aa32a52369d3153179e71a6b
limeojin363 <2002eojin@gmail.com>
2023-09-30 03:06:19 +0900

    chore: 주석 추가

--- src/components/overlays/bottom-sheet/BottomSheetLayout.tsx
+++ src/components/overlays/bottom-sheet/BottomSheetLayout.tsx
@@ -6,6 +6,7 @@ type BottomSheetLayoutProps = {
   onPressBg: () => void;
 };
 
+// useBottomSheet 전용 컴포넌트입니다. 해당 hook 외의 다른 곳에서 사용하지 않도록 주의해주세요.
 const BottomSheetLayout = ({
   children,
   onPressBg,
