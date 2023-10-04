2da6daff8d78011097b96588a3ee72df5bfc36f2
youngchan Kim <aacz12035@gmail.com>
2023-10-04 15:37:23 +0900

    feat: 웹뷰 컴포넌트 헤더 추가

--- src/screens/WebViewScreen.tsx
+++ src/screens/WebViewScreen.tsx
@@ -1,36 +1,58 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import {useConfigContext} from '../hooks/ConfigContext';
 import WebView from 'react-native-webview';
 import webview from '../configs/webview';
 import {StackScreenProps} from '@react-navigation/stack';
+import {ParamListBase} from '@react-navigation/native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import styled from '@emotion/native/dist/emotion-native.cjs';
+import Header from '../components/header/Header';
+import {Text} from 'react-native';
 
-type Props<ParamList extends Record<string, object | undefined>> =
-  StackScreenProps<ParamList>;
+type WebviewProps<NavigationStackParamList extends ParamListBase> =
+  StackScreenProps<Na화vigationStackParamList> & {label?: string};
 
 const WebViewScreen: React.FC<Props<any>> = ({route}) => {
   const {config} = useConfigContext();
   const webviewRef = useRef<WebView>();
   const url = route.params?.url;
+  const insets = useSafeAreaInsets();
+
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
 
   return (
-    <WebView
-      originWhitelist={['https://www.google.com', 'https://m.naver.com']} // In-App Webview를 위한 임시 링크 등록.
-      // originWhitelist={config.get('webview.allowed_hosts') as string[]}
-      // TODO: superbase에 등록된 webview.allowed_hosts에 웹뷰 링크 추가하기.
-      source={{uri: url ?? (config.get('webview.url') as string)}}
-      ref={ref => (webviewRef.current = ref || ({} as WebView))}
-      injectedJavaScript={webview.injectedJavascript}
-      allowsBackForwardNavigationGestures={true}
-      setBuiltInZoomControls={false}
-      setDisplayZoomControls={false}
-      javaScriptEnabled={true}
-      domStorageEnabled={true}
-      allowFileAccess={true}
-      scalesPageToFit={true}
-      cacheEnabled={true}
-      bounces={false}
-    />
+    <S.screenContainer style={{paddingTop: insets.top}}>
+      {label && <Header label={label} onPressBackButton={handleGoBack} />}
+      <WebView
+        originWhitelist={['https://www.google.com', 'https://m.naver.com']} // In-App Webview를 위한 임시 링크 등록.
+        // originWhitelist={config.get('webview.allowed_hosts') as string[]}
+        // TODO: superbase에 등록된 webview.allowed_hosts에 웹뷰 링크 추가하기.
+        source={{uri: url ?? (config.get('webview.url') as string)}}
+        ref={ref => (webviewRef.current = ref || ({} as WebView))}
+        injectedJavaScript={webview.injectedJavascript}
+        allowsBackForwardNavigationGestures={true}
+        setBuiltInZoomControls={false}
+        setDisplayZoomControls={false}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        allowFileAccess={true}
+        scalesPageToFit={true}
+        cacheEnabled={true}
+        bounces={false}
+      />
+    </S.screenContainer>
   );
+};
+
+const S = {
+  screenContainer: styled.View`
+    flex: 1;
+  `,
+  activityIndicatorStyle: styled.View`
+    flex: 1;
+  `,
 };
 
 export default WebViewScreen;
