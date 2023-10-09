58305b7d1608e5edc1ab35a039cf6e4fdebb46a4
limeojin363 <2002eojin@gmail.com>
2023-10-09 22:57:37 +0900

    feat: 슬라이드 애니메이션 적용

--- src/navigators/AnnouncementStackNavigator.tsx
+++ src/navigators/AnnouncementStackNavigator.tsx
@@ -42,21 +42,52 @@ const AnnouncementStackNavigator = () => {
       <Stack.Screen
         name="AnnouncementMain"
         component={AnnouncementMainScreen}
+        options={{...SlideTransition}}
       />
       <Stack.Screen
         name="AnnouncementDetail"
         component={AnnouncementDetailScreen}
+        options={{...SlideTransition}}
       />
       <Stack.Screen
         name="AnnouncementBookmark"
         component={AnnouncementBookmarkBoxScreen}
+        options={{...SlideTransition}}
       />
       <Stack.Screen
         name="AnnouncementSearch"
         component={AnnouncementSearchScreen}
+        options={{...SlideTransition}}
       />
     </Stack.Navigator>
   );
 };
 
 export default AnnouncementStackNavigator;
+
+// 좌우 페이지 이동 구현을 위한 options object: 유튜브 UI를 완전히 따라 구현하기 위해 사용, 일단은 존치.
+// source: https://stackoverflow.com/questions/68731507/react-native-navigation-animation-slide-implementation
+const SlideTransition: StackNavigationOptions = {
+  cardStyleInterpolator: ({current, next, layouts}) => {
+    return {
+      cardStyle: {
+        transform: [
+          {
+            translateX: current.progress.interpolate({
+              inputRange: [0, 1],
+              outputRange: [layouts.screen.width, 0],
+            }),
+          },
+          {
+            translateX: next
+              ? next.progress.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, -layouts.screen.width],
+                })
+              : 1,
+          },
+        ],
+      },
+    };
+  },
+};
