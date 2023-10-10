2d655c8076d21ffae92350126482432fcf35711e
youngchan Kim <aacz12035@gmail.com>
2023-10-10 15:32:40 +0900

    refactor: 앱정보 웹뷰 링크 config url에 추가.

--- src/configs/urls.ts
+++ src/configs/urls.ts
@@ -10,4 +10,10 @@ export default {
         'https://play.google.com/store/apps/details?id=com.nhnent.payapp&hl=ko-KR',
     }),
   },
+  APP_INFORMATION: {
+    //TODO: 앱 정보 웹뷰 임시 링크 대체하기.
+    TO_SAND_POLICIES: 'https://m.blog.naver.com/been_monolid/222866746370',
+    PRIVACY_AND_POLICIES: 'https://www.google.com/',
+    ADVERTISING_AND_MARKETING_CONSENT: 'https://www.youtube.com/?gl=KR&hl=ko',
+  },
 };
