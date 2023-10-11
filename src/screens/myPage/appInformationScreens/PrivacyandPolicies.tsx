7004332008ddd680ab2e0fb026b04c4ef8217e12
youngchan Kim <aacz12035@gmail.com>
2023-10-11 16:41:26 +0900

    refactor: 변경된 웹뷰 컴포넌트 각 개인정보 처리방침 스크린에 적용

--- src/screens/myPage/appInformationScreens/PrivacyandPolicies.tsx
+++ src/screens/myPage/appInformationScreens/PrivacyandPolicies.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {useNavigation, useRoute} from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import {MyPageAppInformationStackParamList} from '../../../navigators/MyPageStackNavigator';
 import {StackNavigationProp} from '@react-navigation/stack';
 import WebViewWithHeader from '../../../components/webView/WebViewWthHeader';
+import URLS from '../../../configs/urls';
 
 type PrivacyPoliciesStackNavigation = StackNavigationProp<
   MyPageAppInformationStackParamList,
@@ -11,12 +12,11 @@ type PrivacyPoliciesStackNavigation = StackNavigationProp<
 
 const PrivacyandPoliciesScreen = () => {
   const navigation = useNavigation<PrivacyPoliciesStackNavigation>();
-  const route = useRoute();
 
   return (
     <WebViewWithHeader
       navigation={navigation}
-      route={route}
+      url={URLS.APP_INFORMATION.PRIVACY_AND_POLICIES}
       label={'개인정보 처리방침'}
     />
   );
