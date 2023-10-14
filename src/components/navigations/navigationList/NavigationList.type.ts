e656062851e6e2d7fddee39363003a7016b7aff1
은배 <geb9211@gmail.com>
2023-10-14 23:52:55 +0900

    feat: navigationList 컴포넌트 수정

--- src/components/navigations/navigationList/NavigationList.type.ts
+++ src/components/navigations/navigationList/NavigationList.type.ts
@@ -1,10 +1,12 @@
+import {colorsType} from '@uoslife/design-system';
 import {PressableProps} from 'react-native';
 
 type NavigationListProps = PressableProps & {
   label: string;
+  pressLabel?: string;
+  pressLabelColor?: colorsType;
+  isPressIconShown?: boolean;
   onPress?: () => void;
-  navigationButton?: React.ReactNode;
-  children?: React.ReactNode;
 };
 
 export default NavigationListProps;
