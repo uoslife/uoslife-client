6aca221919ed42b2769dcb6a967c5a18cbef9d59
은배 <geb9211@gmail.com>
2023-10-15 19:22:17 +0900

    feat: 마이페이지 네비게이션 스택 이름 수정 및 폴더명 변경

--- src/screens/myPage/MypageAppInformationScreen.tsx
+++ src/screens/myPage/MypageAppInformationScreen.tsx
@@ -1,61 +1,53 @@
 import React from 'react';
-import Header from '../../components/header/Header';
-import NavigationList from '../../components/navigations/navigationList/NavigationList';
+import {View} from 'react-native';
 import styled from '@emotion/native';
 import {useNavigation} from '@react-navigation/native';
-import {MypageAppInformationScreenRouteProp} from '../../navigators/MyPageStackNavigator';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import {View} from 'react-native';
+import DeviceInfo from 'react-native-device-info';
 
-type NavigationListType = {
-  label: string;
-  navigate:
-    | 'Mypage_ToSandPolicies'
-    | 'Mypage_privacyPolicies'
-    | 'Mypage_advertisingandMarketingConsent';
-};
-
-const APP_INFORMATION_LIST: NavigationListType[] = [
-  {
-    label: '이용약관 및 정책',
-    navigate: 'Mypage_ToSandPolicies',
-  },
-  {
-    label: '개인정보 처리방침',
-    navigate: 'Mypage_privacyPolicies',
-  },
-  {
-    label: '광고 및 마케팅 수신 동의',
-    navigate: 'Mypage_advertisingandMarketingConsent',
-  },
-];
+import Header from '../../components/header/Header';
+import NavigationList from '../../components/navigations/navigationList/NavigationList';
+import {MypageAppInformationNavigationProp} from '../../navigators/MyPageStackNavigator';
 
 const MypageAppInformationScreen = () => {
   const insets = useSafeAreaInsets();
-  const navigation = useNavigation<MypageAppInformationScreenRouteProp>();
-  const handleGoBack=()=>{
+  const navigation = useNavigation<MypageAppInformationNavigationProp>();
+  const handleGoBack = () => {
     navigation.goBack();
-  }
+  };
 
   return (
     <View style={{paddingTop: insets.top}}>
       <Header label={'앱 정보'} onPressBackButton={handleGoBack} />
       <S.Container>
-        {APP_INFORMATION_LIST.map((item, index) => (
-          <NavigationList
-            key={index}
-            label={item.label}
-            onPress={() => navigation.navigate(item.navigate)}
-          />
-        ))}
+        <NavigationList
+          label="이용약관 및 정책"
+          onPress={() => navigation.navigate('Mypage_ToSandPolicies')}
+        />
+        <NavigationList
+          label="개인정보 처리방침"
+          onPress={() => navigation.navigate('Mypage_privacyPolicies')}
+        />
+        <NavigationList
+          label="광고 및 마케팅 수신 동의"
+          onPress={() =>
+            navigation.navigate('Mypage_advertisingandMarketingConsent')
+          }
+        />
+        <NavigationList
+          label="현재 앱 버전"
+          onPress={() => navigation.navigate('Mypage_ToSandPolicies')}
+          pressLabel={DeviceInfo.getVersion()}
+          isPressIconShown={false}
+        />
       </S.Container>
     </View>
   );
 };
 const S = {
   Container: styled.View`
-    padding: 12px 24px;
-    gap: 24px;
+    width: 100%;
+    padding: 0 16px;
   `,
 };
 
