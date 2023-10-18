f4cf37753730e56cd694f462c42d28434f639943
은배 <geb9211@gmail.com>
2023-10-19 03:41:10 +0900

    feat: 동의 완료 모달 디자인 수정

--- src/components/molecules/account/modalContents/AdvertisingAgreementResult.tsx
+++ src/components/molecules/account/modalContents/AdvertisingAgreementResult.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/native';
-import {Button, Txt} from '@uoslife/design-system';
+import {Button, Txt, colors} from '@uoslife/design-system';
 import ModalInformationBox from '../../../overlays/items/ModalInformationBox';
 
 type Props = {
@@ -11,6 +11,7 @@ const AdvertisingAgreementResult = ({
   isAgree,
   handleClickSubmitModalButton,
 }: Props) => {
+  const date = new Date();
   return (
     <>
       <S.TopContainer>
@@ -18,19 +19,31 @@ const AdvertisingAgreementResult = ({
           label={`광고성 정보 수신동의 ${isAgree ? '처리' : '거부'} 결과`}
           color={'grey190'}
           typograph={'titleMedium'}
+          style={{textAlign: 'center'}}
         />
         <ModalInformationBox
-          informationBoxObject={[{key: '전송자', value: 'uoslife'}]}
+          informationBoxObject={[
+            {key: '전송자', value: `${'uoslife'.toUpperCase()}`},
+            {
+              key: '일시',
+              value: `${date.getFullYear()}년 ${
+                date.getMonth() + 1
+              }월 ${date.getDate()}일`,
+            },
+            {key: '내용', value: `수신동의 ${isAgree ? '처리' : '거부'} 완료`},
+          ]}
         />
         <Txt
           label={`광고성 정보 수신 동의는 MY Page > [앱 설정]에서\n변경 가능합니다.`}
           color={'grey130'}
           typograph={'bodySmall'}
+          style={{textAlign: 'center'}}
         />
       </S.TopContainer>
+      <S.Divider />
       <Button
         label="확인"
-        variant="outline"
+        variant="text"
         isFullWidth
         onPress={handleClickSubmitModalButton}
       />
@@ -45,5 +58,11 @@ const S = {
     display: flex;
     flex-direction: column;
     gap: 8px;
+    padding: 24px 16px 16px;
+  `,
+  Divider: styled.View`
+    width: 100%;
+    height: 1px;
+    background-color: ${colors.grey40};
   `,
 };
