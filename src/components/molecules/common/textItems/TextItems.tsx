70cad3a0acadb5ea0835c6d430597f70cfa38760
은배 <geb9211@gmail.com>
2023-10-18 22:31:01 +0900

    feat: textItem 컴포넌트 작성

---
+++ src/components/molecules/common/textItems/TextItems.tsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/native';
+import {Txt} from '@uoslife/design-system';
+
+type Props = {
+  label: string;
+  item: string;
+};
+
+const TextItems = ({label, item}: Props) => {
+  return (
+    <S.Container>
+      <Txt
+        style={{width: 56}}
+        color={'grey90'}
+        label={label}
+        typograph="titleSmall"
+      />
+      <Txt color={'grey190'} label={item} typograph="bodyLarge" />
+    </S.Container>
+  );
+};
+
+export default TextItems;
+
+const S = {
+  Container: styled.View`
+    flex-direction: row;
+    gap: 24px;
+    align-items: center;
+    justify-content: flex-start;
+    width: 100%;
+  `,
+};
