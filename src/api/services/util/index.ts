0a1b2fd7240a1a214e508d8aacd6c73afe672c69
은배 <geb9211@gmail.com>
2023-10-20 09:26:34 +0900

    feat: 누락된 libraryAPI export 추가

--- src/api/services/util/index.ts
+++ src/api/services/util/index.ts
@@ -1,10 +1,12 @@
 import CafeteriaAPI from './cafeteria/cafeteriaAPI';
 import AnnouncementAPI from './announcement/announcementAPI';
+import LibraryAPI from './library/libraryAPI';
 import StudentIdAPI from './studentId/studentIdAPI';
 
 const UtilAPI = {
   ...CafeteriaAPI,
   ...AnnouncementAPI,
+  ...LibraryAPI,
   ...StudentIdAPI,
 };
 
