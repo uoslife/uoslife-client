db676c37280c9010a96d8833d497ecd0cad95354
limeojin363 <2002eojin@gmail.com>
2023-10-20 15:24:23 +0900

    feat: 원본 글 보기 버튼 추가

--- src/components/molecules/announcement/announcement-detail/AnnouncementContent.tsx
+++ src/components/molecules/announcement/announcement-detail/AnnouncementContent.tsx
@@ -1,42 +1,60 @@
-import {View} from 'react-native';
+import {Alert, Linking, Pressable, Touchable, View} from 'react-native';
 import {ArticleDetailType} from '../../../../types/announcement.type';
 import AnnouncementDetailScreenBookmarkToggle from './AnnouncementDetailScreenBookmarkToggle';
-import {Txt, colors} from '@uoslife/design-system';
+import {Button, Txt, colors} from '@uoslife/design-system';
 import {announcementFullName} from '../../../../configs/announcement';
-  import styled from '@emotion/native';
-  import AnnouncementFileList from './AnnouncementFileList';
-  import AnnouncementHTML from './AnnouncementHTML';
+import styled from '@emotion/native';
+import AnnouncementFileList from './AnnouncementFileList';
+import AnnouncementHTML from './AnnouncementHTML';
+import {Link} from '@react-navigation/native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import IconWithText from '../../iconWithText/IconWithText';
 
-  const AnnouncementDetailScreenContent = ({
-    title,
-    bookmarkCount,
-    date,
-    description,
-    files,
-    origin,
-  }: ArticleDetailType) => {
-    return (
-      <S.Root>
-        <View style={{borderBottomColor: colors.grey20, borderBottomWidth: 1}}>
-          <S.TopWrapper>
-            <Txt label={title} color={'black'} typograph={'titleLarge'} />
-            <S.CategoryAndDateAndBookmarkContainer>
-              <Txt
-                label={`${announcementFullName[origin]} | ${date}`}
-                color={'grey90'}
-                typograph={'bodySmall'}
-              />
+const AnnouncementDetailScreenContent = ({
+  title,
+  bookmarkCount,
+  date,
+  description,
+  files,
+  origin,
+  url,
+}: ArticleDetailType) => {
+  const GoToOriginUrl = () => {
+    Linking.openURL(url);
+  };
+
+  return (
+    <S.Root>
+      <View style={{borderBottomColor: colors.grey20, borderBottomWidth: 1}}>
+        <S.TopWrapper>
+          <Txt label={title} color={'black'} typograph={'titleLarge'} />
+
+          <S.CategoryAndDateAndBookmarkContainer>
+            <Txt
+              label={`${announcementFullName[origin]} | ${date}`}
+              color={'grey90'}
+              typograph={'bodySmall'}
+            />
+            <View style={{flexDirection: 'row', alignItems: 'center', gap: 8}}>
+              <S.GoToOriginUrl onPress={GoToOriginUrl}>
+                <Txt
+                  color={'grey90'}
+                  label={'원본 글 보기'}
+                  typograph={'bodyLarge'}
+                />
+              </S.GoToOriginUrl>
               <AnnouncementDetailScreenBookmarkToggle
                 bookmarkCount={bookmarkCount}
               />
-            </S.CategoryAndDateAndBookmarkContainer>
-          </S.TopWrapper>
-        </View>
-        {files.length != 0 && <AnnouncementFileList files={files} />}
-        <AnnouncementHTML description={description} />
-      </S.Root>
-    );
-  };
+            </View>
+          </S.CategoryAndDateAndBookmarkContainer>
+        </S.TopWrapper>
+      </View>
+      {files.length != 0 && <AnnouncementFileList files={files} />}
+      <AnnouncementHTML description={description} />
+    </S.Root>
+  );
+};
 
 export default AnnouncementDetailScreenContent;
 
@@ -51,6 +69,15 @@ const S = {
 
     border-bottom: 1px ${colors.black};
     border-color: ${colors.black};
+  `,
+  GoToOriginUrl: styled.TouchableOpacity`
+    align-items: center;
+    justify-content: center;
+
+    padding: 6px 12px 6px 12px;
+    border-radius: 10px;
+
+    border: 1px ${colors.grey40};
   `,
   CategoryAndDateAndBookmarkContainer: styled.View`
     display: flex;
