e89484bbfc442dcaa1e4fb6a5252fd251b014c44
youngchan Kim <aacz12035@gmail.com>
2023-10-30 13:52:50 +0900

    fix: 코드 번잡화 방지를 위한 StackNavigation type 변경

--- src/screens/myPage/appInformation/ToSandPolicies.tsx
+++ src/screens/myPage/appInformation/ToSandPolicies.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
-import {MyPageAppInformationStackParamList} from '../../../navigators/MyPageStackNavigator';
-import {StackNavigationProp} from '@react-navigation/stack';
 import WebViewWithHeader from '../../../components/webView/WebViewWthHeader';
 import URLS from '../../../configs/urls';
 
-type ToSandPoliciesStackNavigation = StackNavigationProp<
-  MyPageAppInformationStackParamList,
-  'Mypage_ToSandPolicies'
->;
-
 const ToSandPoliciesScreen = () => {
-  const navigation = useNavigation<ToSandPoliciesStackNavigation>();
+  const navigation = useNavigation<any>();
 
   return (
     <WebViewWithHeader
