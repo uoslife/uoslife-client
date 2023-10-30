c2f78e5286424032dbf31961a480c7c2f71575d9
youngchan Kim <aacz12035@gmail.com>
2023-10-30 14:32:14 +0900

    fix: 코드 번잡화 방지를 위한 StackNavigation type 변경

--- src/screens/myPage/appInformation/AdvertisingandMarketingConsentScreen.tsx
+++ src/screens/myPage/appInformation/AdvertisingandMarketingConsentScreen.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
-import {MyPageAppInformationStackParamList} from '../../../navigators/MyPageStackNavigator';
-import {StackNavigationProp} from '@react-navigation/stack';
 import WebViewWithHeader from '../../../components/webView/WebViewWthHeader';
 import URLS from '../../../configs/urls';
 
-type AdvertisingandMarketingConsentNavigationStack = StackNavigationProp<
-  MyPageAppInformationStackParamList,
-  'Mypage_advertisingandMarketing'
->;
-
 const ToSandPoliciesScreen = () => {
-  const navigation =
-    useNavigation<AdvertisingandMarketingConsentNavigationStack>();
+  const navigation = useNavigation<any>();
 
   return (
     <WebViewWithHeader
--- src/screens/myPage/appInformation/PrivacyandPolicies.tsx
+++ src/screens/myPage/appInformation/PrivacyandPolicies.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
-import {MyPageAppInformationStackParamList} from '../../../navigators/MyPageStackNavigator';
-import {StackNavigationProp} from '@react-navigation/stack';
 import WebViewWithHeader from '../../../components/webView/WebViewWthHeader';
 import URLS from '../../../configs/urls';
 
-type PrivacyPoliciesStackNavigation = StackNavigationProp<
-  MyPageAppInformationStackParamList,
-  'Mypage_privacyPolicies'
->;
-
 const PrivacyandPoliciesScreen = () => {
-  const navigation = useNavigation<PrivacyPoliciesStackNavigation>();
+  const navigation = useNavigation<any>();
 
   return (
     <WebViewWithHeader
