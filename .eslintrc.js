c2b79485826fded5393dc7ca30f28a792b8a5062
은배 <geb9211@gmail.com>
2023-11-03 15:17:13 +0900

    chore: eslint 관련 패키지 설치 및 환경설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,39 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community', 'plugin:storybook/recommended'],
+  extends: [
+    '@react-native-community',
+    'airbnb',
+    'airbnb-typescript',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+    'eslint-config-prettier',
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    project: './tsconfig.json',
+  },
   rules: {
+    'import/no-unresolved': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', {variables: false}],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.ts', '.tsx'],
+      },
+    ],
+    'import/no-extraneous-dependencies': 'off',
+    'global-require': 'off',
+    'import/prefer-default-export': ['warn', {target: 'single'}],
+    '@typescript-eslint/return-await': 'off',
+    'react/function-component-definition': [
+      2,
+      {namedComponents: 'arrow-function'},
+    ],
+    'react/react-in-jsx-scope': 'off',
     curly: 0,
   },
+  plugins: ['prettier'],
+  ignorePatterns: ['**/storybook-static'],
 };
