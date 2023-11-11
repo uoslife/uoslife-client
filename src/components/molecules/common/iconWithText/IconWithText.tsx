24360691f851a93d26cfdf01eb51905785f810e2
은배 <geb9211@gmail.com>
2023-11-12 06:47:13 +0900

    feat: iconWithText 컴포넌트 prop 확장

--- src/components/molecules/common/iconWithText/IconWithText.tsx
+++ src/components/molecules/common/iconWithText/IconWithText.tsx
@@ -1,27 +1,28 @@
 import styled from '@emotion/native';
 import {Icon, IconsNameType, Txt, colorsType} from '@uoslife/design-system';
 import React from 'react';
+import {PressableProps} from 'react-native';
 
 type Props = {
   iconName: IconsNameType;
   text: string;
   color?: colorsType;
   isClick?: boolean;
-};
+} & PressableProps;
 
-const IconWithText = ({iconName, color, text, isClick}: Props) => {
+const IconWithText = ({iconName, color, text, isClick, ...props}: Props) => {
   return (
-    <S.Wrapper>
+    <S.Wrapper {...props}>
       <Icon
         name={iconName}
         width={24}
         height={24}
-        color={isClick ? 'primaryBrand' : 'grey190' ?? color}
+        color={isClick ? 'primaryBrand' : 'grey90' ?? color}
       />
       <Txt
         label={text}
-        color={isClick ? 'primaryBrand' : 'grey160' ?? color}
-        typograph="caption"
+        color={isClick ? 'primaryBrand' : 'grey60' ?? color}
+        typograph="labelLarge"
       />
     </S.Wrapper>
   );
