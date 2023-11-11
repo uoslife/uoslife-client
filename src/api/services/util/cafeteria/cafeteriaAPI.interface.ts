41705b1552a7f37bb062307e43b8a8b24c1f03e1
은배 <geb9211@gmail.com>
2023-11-12 06:47:23 +0900

    feat: 학식 APi 수정

--- src/api/services/util/cafeteria/cafeteriaAPI.interface.ts
+++ src/api/services/util/cafeteria/cafeteriaAPI.interface.ts
@@ -2,12 +2,8 @@ import {ServiceFunc} from '../../type';
 import * as Type from './cafeteriaAPI.type';
 
 export default interface NotificationService {
-  getCafeteriasWithId: ServiceFunc<
-    Type.GetCafeteriasWithIdParams,
-    Type.GetCafeteriasWithIdResponse
-  >;
-  getCafeteriasWithDate: ServiceFunc<
-    Type.GetCafeteriasWithDateParams,
-    Type.GetCafeteriasWithDateResponse
+  getCafeterias: ServiceFunc<
+    Type.GetCafeteriasParams,
+    Type.GetCafeteriasResponse
   >;
 }
