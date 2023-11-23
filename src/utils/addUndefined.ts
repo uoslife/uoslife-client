f24d94e19dcfe39a22d7ff3576e9235e93900b27
EunBae Gong <geb9211@gmail.com>
2023-11-24 03:45:13 +0900

    feat: 사용 예시 주석 추가

--- src/utils/addUndefined.ts
+++ src/utils/addUndefined.ts
@@ -1,4 +1,12 @@
-/** object에 undefined type을 추가합니다. */
+/**
+ * object에 undefined type을 추가합니다.
+ * #### Example
+ *
+ * ```ts
+ * type Object = {foo: 1};
+ * type ObjectAddedUndefined = AddUndefined<Object> // {foo?: 1}
+ * ```
+ */
 type AddUndefined<T> = {[K in keyof T]?: T[K]};
 
 export default AddUndefined;
