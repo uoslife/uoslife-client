eaa4b23ceede5e0175e546ada42f0f4f781b3ed4
EunBae Gong <geb9211@gmail.com>
2023-11-26 05:44:24 +0900

    feat: 토스트 메세지 문구 변경

--- src/configs/toast/toastMessageProps.tsx
+++ src/configs/toast/toastMessageProps.tsx
@@ -1,9 +1,10 @@
 import {ShowToastProps} from '.';
 
 const toastMessage = {
-  logout: '로그아웃에 성공했습니다.',
-  unregister: '회원탈퇴에 성공했습니다.',
-  unregisterError: '회원탈퇴를 처리하는 중 오류가 발생했습니다.',
+  logout: '로그아웃에 성공했어요.',
+  unregister: '회원탈퇴에 성공했어요.',
+  unregisterError: '회원탈퇴를 처리하는 중 오류가 발생했어요.',
+  notLoggedInError: '로그인 후 이용 가능해요.',
 };
 export type ToastMessageType = keyof typeof toastMessage;
 
@@ -17,6 +18,10 @@ const toastMessageProps: {[T in ToastMessageType]: ShowToastProps} = {
   unregisterError: {
     type: 'error',
     title: toastMessage.unregisterError,
+  },
+  notLoggedInError: {
+    type: 'error',
+    title: toastMessage.notLoggedInError,
   },
 };
 export default toastMessageProps;
