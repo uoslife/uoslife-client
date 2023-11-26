4c4754d27981d4d82192c16c8a4b5326d86fa033
EunBae Gong <geb9211@gmail.com>
2023-11-26 16:59:56 +0900

    feat: toast 문구 추가

--- src/configs/toast/toastMessageProps.tsx
+++ src/configs/toast/toastMessageProps.tsx
@@ -3,8 +3,12 @@ import {ShowToastProps} from '.';
 const toastMessage = {
   logout: '로그아웃에 성공했어요.',
   unregister: '회원탈퇴에 성공했어요.',
-  unregisterError: '회원탈퇴를 처리하는 중 오류가 발생했어요.',
+  unregisterError: '회원탈퇴를 처리하는 중 문제가 발생했어요.',
   notLoggedInError: '로그인 후 이용 가능해요.',
+  changeNickname: '닉네임 변경에 성공했어요.',
+  changeNicknameError: '닉네임을 변경하는 중 문제가 발생했어요.',
+  changePhone: '전화번호 변경에 성공했어요.',
+  changePhoneError: '전화번호를 변경하는 중 문제가 발생했어요.',
 };
 export type ToastMessageType = keyof typeof toastMessage;
 
@@ -22,6 +26,20 @@ const toastMessageProps: {[T in ToastMessageType]: ShowToastProps} = {
   notLoggedInError: {
     type: 'error',
     title: toastMessage.notLoggedInError,
+  },
+  changeNickname: {
+    title: toastMessage.changeNickname,
+  },
+  changeNicknameError: {
+    type: 'error',
+    title: toastMessage.changeNicknameError,
+  },
+  changePhone: {
+    title: toastMessage.changePhone,
+  },
+  changePhoneError: {
+    type: 'error',
+    title: toastMessage.changePhoneError,
   },
 };
 export default toastMessageProps;
