37d49b6db8d0b531e985157b8e558e8cc21abb75
limeojin363 <2002eojin@gmail.com>
2023-11-29 14:06:34 +0900

    fix: useCallback 적용을 통한, Detail 페이지에서의 무한 리렌더링 해결

--- src/hooks/useBookmarkOnLocal.tsx
+++ src/hooks/useBookmarkOnLocal.tsx
@@ -4,22 +4,24 @@
     개별 endpoint에서 북마크 정보를 받아오도록 고치기
  */
 
+import {useCallback} from 'react';
 import BookmarkAPI from '../api/services/util/bookmark/bookmarkAPI';
 import storage from '../storage';
 
 const BOOKMARK_ID_LIST_LITERAL = 'bookmark-id-list';
 
 const useBookmarkOnLocal = () => {
-  const loadBookmarkOnLocal = () => {
+  const loadBookmarkOnLocal = useCallback(() => {
     const idListJson = storage.getString(BOOKMARK_ID_LIST_LITERAL);
     return idListJson ? (JSON.parse(idListJson) as number[]) : null;
-  };
-  const saveBookmarkOnLocal = (bookmarkIdList: number[]) => {
+  }, []);
+
+  const saveBookmarkOnLocal = useCallback((bookmarkIdList: number[]) => {
     storage.set(BOOKMARK_ID_LIST_LITERAL, JSON.stringify(bookmarkIdList));
-  };
+  }, []);
 
   /** 북마크 정보를 로컬에서 가져와 반환, 없다면 새로 요청해서 반환 */
-  const getBookmarkIdList = async () => {
+  const getBookmarkIdList = useCallback(async () => {
     const loadedFromLocal = loadBookmarkOnLocal();
     if (loadedFromLocal) {
       return loadedFromLocal;
@@ -33,7 +35,7 @@ const useBookmarkOnLocal = () => {
     saveBookmarkOnLocal(idList);
 
     return idList;
-  };
+  }, [loadBookmarkOnLocal, saveBookmarkOnLocal]);
 
   return {
     saveBookmarkOnLocal,
