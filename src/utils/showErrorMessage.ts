6dc92260314fd7b05dd85dc52b43ac079ab095fe
EunBae Gong <geb9211@gmail.com>
2023-12-23 23:56:29 +0900

    feat: 사용하지 않는 util 함수 삭제

--- src/utils/showErrorMessage.ts
+++
@@ -1,8 +0,0 @@
-import {Alert} from 'react-native';
-
-const showErrorMessage = (error: unknown) => {
-  console.error(error);
-  Alert.alert('알 수 없는 오류', '이용에 불편을 드려 죄송합니다.');
-};
-
-export default showErrorMessage;
