e3a89376e80f23d3dd1881e0d722de7bb42b6b89
EunBae Gong <geb9211@gmail.com>
2023-12-28 06:09:15 +0900

    fix: access token이 재발급 되지 않는 현상 해결

--- src/api/core/afterResponse.ts
+++ src/api/core/afterResponse.ts
@@ -3,24 +3,28 @@ import {CoreAPI} from '../services';
 import storage from '../../storage';
 import UserService from '../../services/user';
 import storeToken from '../../utils/storeToken';
+import apiClient from './client';
+import customShowToast from '../../configs/toast';
 
 const handleToken: AfterResponseHook = async (request, _options, response) => {
-  const refreshTokenInStorage = storage.getString('refreshToken');
   if (response.status !== 401 || request.url.includes('refresh')) {
     return response;
   }
+
+  // refresh token을 이용한 access token 재발급
   try {
-    request.headers.set('Authorization', `Bearer ${refreshTokenInStorage}`);
     const res = await CoreAPI.getRefreshToken({});
     if (res) {
       const {accessToken, refreshToken} = res;
       storeToken({accessToken, refreshToken});
-      return ky(request);
+      return apiClient(request);
     }
   } catch (error) {
     await UserService.logout({});
     storage.set('isLoggedIn', false);
+    customShowToast('loginDurationExpiredInfo');
     return response;
   }
 };
+
 export default handleToken;
