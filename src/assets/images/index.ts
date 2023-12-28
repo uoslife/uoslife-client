e415390de554282ab1f6bb6558ea600fd7840d85
EunBae Gong <geb9211@gmail.com>
2023-12-29 05:43:58 +0900

    design: 숨은 학점 온보딩 이미지 추가

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -4,3 +4,9 @@ export const ONBOARDING_3 = require('./onboarding/onboarding_3.png');
 
 export const BANNER_1 = require('./banner/banner_1.png');
 export const BANNER_2 = require('./banner/banner_2.png');
+export const BANNER_3 = require('./banner/banner_3.png');
+
+export const MYGRADE_ONBOARDING_1 = require('./mygradeOnboarding/mygrade_onboarding_1.png');
+export const MYGRADE_ONBOARDING_2 = require('./mygradeOnboarding/mygrade_onboarding_2.png');
+export const MYGRADE_ONBOARDING_3 = require('./mygradeOnboarding/mygrade_onboarding_3.png');
+export const MYGRADE_ONBOARDING_4 = require('./mygradeOnboarding/mygrade_onboarding_4.png');
