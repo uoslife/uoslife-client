8e233e7b4de5c5f8743d48dfe3d8c443bf922646
EunBae Gong <geb9211@gmail.com>
2024-01-30 07:19:04 +0900

    feat: 배너이미지 추가

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -5,6 +5,7 @@ export const ONBOARDING_3 = require('./onboarding/onboarding_3.png');
 export const BANNER_1 = require('./banner/banner_1.png');
 export const BANNER_2 = require('./banner/banner_2.png');
 export const BANNER_3 = require('./banner/banner_3.png');
+export const BANNER_4 = require('./banner/banner_4.png');
 
 export const MYGRADE_ONBOARDING_1 = require('./mygradeOnboarding/mygrade_onboarding_1.png');
 export const MYGRADE_ONBOARDING_2 = require('./mygradeOnboarding/mygrade_onboarding_2.png');
