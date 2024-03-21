10565bc3788b2de5eb98d9e09dc9a01ab25a66de
EunBae Gong <geb9211@gmail.com>
2024-03-22 05:24:43 +0900

    fix: 다른 화면에서도 QR API가 호출되는 경우 수정

--- src/components/molecules/screens/studentId/StudentIdQrCode.tsx
+++ src/components/molecules/screens/studentId/StudentIdQrCode.tsx
@@ -1,48 +1,22 @@
-import React, {useEffect, useState} from 'react';
+import {useIsFocused} from '@react-navigation/native';
 import {useSuspenseQuery} from '@tanstack/react-query';
-import {AppState} from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
-import {useIsFocused, useNavigation} from '@react-navigation/native';
+
 import {UtilAPI} from '../../../../api/services';
+import useRefreshOnFocus from '../../../../hooks/useRefreshOnFocus';
+
+const DEFAULT_QRCODE_REFETCH_INTERVAL = 10 * 1000;
 
 const StudentIdQrCode = () => {
   const isFocused = useIsFocused();
-  const navigation = useNavigation();
-  const [refetchInterval, setRefetchInterval] = useState<number | false>(
-    1000 * 10,
-  );
-
   const {data, refetch} = useSuspenseQuery({
     queryKey: ['qrCode'],
     queryFn: () => UtilAPI.getStudentId({}),
-    refetchInterval,
+    refetchInterval: isFocused ? DEFAULT_QRCODE_REFETCH_INTERVAL : false,
     select: qrCode => qrCode.data,
   });
 
-  const handleAppStateChange = async () => {
-    if (isFocused) {
-      await refetch();
-      setRefetchInterval(1000 * 10);
-      return;
-    }
-    setRefetchInterval(false);
-  };
-
-  useEffect(() => {
-    // 여러 navigation(메인, 숨은학점 등 스크린) 전환 시,
-    const blurListener = navigation.addListener('blur', () =>
-      handleAppStateChange(),
-    );
-    // background에서 foreground로 전환 시,
-    const appStateListener = AppState.addEventListener('change', () =>
-      handleAppStateChange(),
-    );
-    return () => {
-      navigation.removeListener('blur', blurListener);
-      appStateListener.remove();
-    };
-  }, [isFocused, AppState.currentState]);
-
+  useRefreshOnFocus(refetch);
   return <QRCode value={data} size={140} logoBackgroundColor="transparent" />;
 };
 
