760f0d9e209e159b8176a502aebd1507bf554dfb
EunBae Gong <geb9211@gmail.com>
2024-03-22 01:02:31 +0900

    feat: 포털 연동 API 수정 및 추가

--- src/api/services/core/verification/verificationAPI.ts
+++ src/api/services/core/verification/verificationAPI.ts
@@ -1,9 +1,18 @@
-import {post} from '../../../core/methods';
-import verificationService from './verificationAPI.interface';
+import {del, get, post} from '../../../core/methods';
+import VerificationService from './verificationAPI.interface';
 import * as Type from './verificationAPI.type';
 
-const verificationAPI: verificationService = {
+const verificationAPI: VerificationService = {
   portalVerification: params =>
     post<Type.PortalVerificationRes>('core/verification', params),
+  getPortalVerification: () =>
+    get<Type.GetPortalVerificationRes>('core/verification'),
+  deletePortalVerification: () =>
+    del<Type.DeletePortalVerificationRes>('core/verification'),
+  representativePortalVerification: params =>
+    post<Type.RepresentativePortalVerificationRes>(
+      'core/verification/representative',
+      params,
+    ),
 };
 export default verificationAPI;
