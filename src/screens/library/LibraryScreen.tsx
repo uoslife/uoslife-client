d5df6f8e2acfe33487dde45bb16c0d8656187982
EunBae Gong <geb9211@gmail.com>
2024-03-24 00:49:45 +0900

    feat: 새로고침 기능 추가

--- src/screens/library/LibraryScreen.tsx
+++ src/screens/library/LibraryScreen.tsx
@@ -1,14 +1,12 @@
 import styled from '@emotion/native';
-import {Suspense} from 'react';
-
 import {ScrollView} from 'react-native-gesture-handler';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {useNavigation} from '@react-navigation/native';
+
 import LibraryUserInfo from '../../components/molecules/screens/library/LibraryUserInfo';
 import LibrarySeatStatus from '../../components/molecules/screens/library/LibararySeatStatus';
 import Header from '../../components/molecules/common/header/Header';
 import LibraryUsageHistory from '../../components/molecules/screens/library/LibraryUsageHistory';
-import Skeleton from '../../components/molecules/common/skeleton/Skeleton';
 
 const LibraryScreen = () => {
   const insets = useSafeAreaInsets();
@@ -23,9 +21,7 @@ const LibraryScreen = () => {
       <S.ScreenContainer>
         <Header label="도서관" onPressBackButton={handleGoBack} />
         <LibraryUserInfo />
-        <Suspense fallback={<Skeleton variant="card" />}>
-          <LibraryUsageHistory />
-        </Suspense>
+        <LibraryUsageHistory />
         <LibrarySeatStatus />
       </S.ScreenContainer>
     </ScrollView>
