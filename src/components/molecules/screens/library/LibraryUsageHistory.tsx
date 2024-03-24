8d4385919c26966dd9dd2c99907b601bdc19fe18
EunBae Gong <76601773+eunbae0@users.noreply.github.com>
2024-03-24 23:02:31 +0900

    [APP-268] 올해 도서관 이용내역 서비스 새로고침 구현 (#323)
    ## Key Changes
    - 올해 도서관 이용내역 서비스의 새로고침 기능을 구현했습니다.
    
    ## Details
    - 이용내역을 호출하는 방식을 react query를 이용하도록 변경했습니다.
    
    ## Allocated Issue
    - close #314

--- src/components/molecules/screens/library/LibraryUsageHistory.tsx
+++ src/components/molecules/screens/library/LibraryUsageHistory.tsx
@@ -1,86 +1,96 @@
+import {Suspense} from 'react';
+import {Pressable} from 'react-native';
 import styled from '@emotion/native';
 import {Icon, Txt} from '@uoslife/design-system';
+import {useSuspenseQuery} from '@tanstack/react-query';
 
-import {useEffect, useState} from 'react';
 import CardLayout from '../../common/cardLayout/CardLayout';
 import UtilityService from '../../../../services/utility';
-import {RecapInfoType} from '../../../../api/services/core/libraryHistory/libraryHistoryAPI.type';
+import Skeleton from '../../common/skeleton/Skeleton';
 
 const LibraryUsageHistory = () => {
-  const [LibraryHistory, setLibraryHistory] = useState<RecapInfoType | null>(
-    null,
-  );
-
-  useEffect(() => {
-    (async () => {
-      const libraryStatusRes = await UtilityService.getLibraryUsageStatus();
-      setLibraryHistory(libraryStatusRes);
-    })();
-  }, []);
+  const {data, refetch} = useSuspenseQuery({
+    queryKey: ['getLibraryUsageStatus'],
+    queryFn: () => UtilityService.getLibraryUsageStatus(),
+  });
 
   return (
     <S.Container>
-      <Txt
-        label="올해의 도서관 이용 내역"
-        color="grey190"
-        typograph="titleLarge"
-        style={{paddingLeft: 8}}
-      />
-      <S.LibraryHistoryCardWrapper>
-        {!LibraryHistory ? (
-          <CardLayout style={{padding: 20, paddingLeft: 16, gap: 6}}>
-            <Txt
-              label="정보를 불러올 수 없어요."
-              color="grey150"
-              typograph="titleSmall"
-            />
-            <Txt
-              label="서버의 문제이거나 졸업생이면 확인할 수 없어요"
-              color="grey90"
-              typograph="caption"
-            />
-          </CardLayout>
-        ) : (
-          <>
-            <CardLayout>
-              <S.CardContainer>
-                <S.CardLeftWrapper>
-                  <Icon name="calendar" width={20} height={20} />
-                  <Txt label="출석일" color="grey90" typograph="titleSmall" />
-                </S.CardLeftWrapper>
-                <S.CardTextWrapper>
-                  <Txt
-                    label={LibraryHistory.usageTime.useTime.toString()}
-                    color="primaryBrand"
-                    typograph="headlineMedium"
-                  />
-                  <Txt
-                    label="/365일"
-                    color="grey90"
-                    typograph="headlineMedium"
-                  />
-                </S.CardTextWrapper>
-              </S.CardContainer>
+      <S.TitleWrapper>
+        <Txt
+          label="올해의 도서관 이용 내역"
+          color="grey190"
+          typograph="titleLarge"
+        />
+        <Pressable onPress={() => refetch()}>
+          <Icon name="refresh" width={24} height={24} color="grey90" />
+        </Pressable>
+      </S.TitleWrapper>
+      <Suspense fallback={<Skeleton variant="card" />}>
+        <S.LibraryHistoryCardWrapper>
+          {!data ? (
+            <CardLayout style={{padding: 20, paddingLeft: 16, gap: 6}}>
+              <Txt
+                label="정보를 불러올 수 없어요."
+                color="grey150"
+                typograph="titleSmall"
+              />
+              <Txt
+                label="서버의 문제이거나 졸업생이면 확인할 수 없어요."
+                color="grey90"
+                typograph="caption"
+              />
             </CardLayout>
-            <CardLayout>
-              <S.CardContainer>
-                <S.CardLeftWrapper>
-                  <Icon name="alarm" width={20} height={20} />
-                  <Txt label="이용시간" color="grey90" typograph="titleSmall" />
-                </S.CardLeftWrapper>
-                <S.CardTextWrapper>
-                  <Txt
-                    label={LibraryHistory.usageTime.useHour.toString()}
-                    color="primaryBrand"
-                    typograph="headlineMedium"
-                  />
-                  <Txt label="시간" color="grey90" typograph="headlineMedium" />
-                </S.CardTextWrapper>
-              </S.CardContainer>
-            </CardLayout>
-          </>
-        )}
-      </S.LibraryHistoryCardWrapper>
+          ) : (
+            <>
+              <CardLayout>
+                <S.CardContainer>
+                  <S.CardLeftWrapper>
+                    <Icon name="calendar" width={20} height={20} />
+                    <Txt label="출석일" color="grey90" typograph="titleSmall" />
+                  </S.CardLeftWrapper>
+                  <S.CardTextWrapper>
+                    <Txt
+                      label={data.usageTime.useTime.toString()}
+                      color="primaryBrand"
+                      typograph="headlineMedium"
+                    />
+                    <Txt
+                      label="/365일"
+                      color="grey90"
+                      typograph="headlineMedium"
+                    />
+                  </S.CardTextWrapper>
+                </S.CardContainer>
+              </CardLayout>
+              <CardLayout>
+                <S.CardContainer>
+                  <S.CardLeftWrapper>
+                    <Icon name="alarm" width={20} height={20} />
+                    <Txt
+                      label="이용시간"
+                      color="grey90"
+                      typograph="titleSmall"
+                    />
+                  </S.CardLeftWrapper>
+                  <S.CardTextWrapper>
+                    <Txt
+                      label={data.usageTime.useHour.toString()}
+                      color="primaryBrand"
+                      typograph="headlineMedium"
+                    />
+                    <Txt
+                      label="시간"
+                      color="grey90"
+                      typograph="headlineMedium"
+                    />
+                  </S.CardTextWrapper>
+                </S.CardContainer>
+              </CardLayout>
+            </>
+          )}
+        </S.LibraryHistoryCardWrapper>
+      </Suspense>
     </S.Container>
   );
 };
@@ -102,7 +112,13 @@ const S = {
     align-items: center;
     padding: 20px 16px;
   `,
-
+  TitleWrapper: styled.View`
+    padding: 0 5px 0 8px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  `,
   CardLeftWrapper: styled.View`
     gap: 4px;
     flex-direction: row;
