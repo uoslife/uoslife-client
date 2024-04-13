a9f6d8714a47d266a677d8d9531e2bd0a4993903
EunBae Gong <76601773+eunbae0@users.noreply.github.com>
2024-04-14 02:04:24 +0900

    [RN-22] 버튼 애니메이션 추가 (#377)
    # Key Changes
    
    - moti를 이용해 버튼 애니메이션을 적용합니다.
    
    # Closes Issue
    
    close #375

--- src/components/animations/pressable_icon/AnimatePress.tsx
+++ src/components/animations/pressable_icon/AnimatePress.tsx
@@ -1,66 +1,62 @@
-// import {useMemo} from 'react';
-import {Pressable, PressableProps} from 'react-native';
-// import {
-//   MotiPressable,
-//   MotiPressableProps,
-//   MotiPressableInteractionState,
-// } from 'moti/interactions';
+import {useMemo} from 'react';
+import {
+  MotiPressable,
+  MotiPressableProps,
+  MotiPressableInteractionState,
+} from 'moti/interactions';
 
 type AnimateVariant = 'scale_up' | 'scale_up_2' | 'scale_down';
-type Props = {variant: AnimateVariant} & PressableProps;
+type Props = {variant: AnimateVariant} & MotiPressableProps;
 
 const AnimatePress = ({children, variant, ...props}: Props) => {
-  // const scaleUp = useMemo(
-  //   () =>
-  //     ({pressed}: MotiPressableInteractionState) => {
-  //       'worklet';
+  const scaleUp = useMemo(
+    () =>
+      ({pressed}: MotiPressableInteractionState) => {
+        'worklet';
 
-  //       return {
-  //         scale: pressed ? 1.2 : 1,
-  //       };
-  //     },
-  //   [],
-  // );
-  // const scaleUp2 = useMemo(
-  //   () =>
-  //     ({pressed}: MotiPressableInteractionState) => {
-  //       'worklet';
+        return {
+          scale: pressed ? 1.2 : 1,
+        };
+      },
+    [],
+  );
+  const scaleUp2 = useMemo(
+    () =>
+      ({pressed}: MotiPressableInteractionState) => {
+        'worklet';
 
-  //       return {
-  //         scale: pressed ? 1.1 : 1,
-  //       };
-  //     },
-  //   [],
-  // );
-  // const scaleDown = useMemo(
-  //   () =>
-  //     ({pressed}: MotiPressableInteractionState) => {
-  //       'worklet';
+        return {
+          scale: pressed ? 1.1 : 1,
+        };
+      },
+    [],
+  );
+  const scaleDown = useMemo(
+    () =>
+      ({pressed}: MotiPressableInteractionState) => {
+        'worklet';
 
-  //       return {
-  //         scale: pressed ? 0.96 : 1,
-  //       };
-  //     },
-  //   [],
-  // );
+        return {
+          scale: pressed ? 0.96 : 1,
+        };
+      },
+    [],
+  );
 
-  // const switchVariant = () => {
-  //   switch (variant) {
-  //     case 'scale_up':
-  //     return scaleUp;
-  //     case 'scale_up_2':
-  //       return scaleUp2;
-  //     case 'scale_down':
-  //       return scaleDown;
-  //   }
-  // };
+  const switchVariant = () => {
+    switch (variant) {
+      case 'scale_up':
+        return scaleUp;
+      case 'scale_up_2':
+        return scaleUp2;
+      case 'scale_down':
+        return scaleDown;
+    }
+  };
   return (
-    <Pressable
-      {...props}
-      //  animate={switchVariant()}
-    >
+    <MotiPressable {...props} animate={switchVariant()}>
       {children}
-    </Pressable>
+    </MotiPressable>
   );
 };
 
