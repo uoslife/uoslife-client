27529e2f50e84d2daa9d1920e4dd1171e43c656b
jongbin26 <010jjj@naver.com>
2024-04-17 00:29:08 +0900

    feat: 도서관 안내 메시지 팝업 추가

---
+++ src/components/molecules/common/GuidePopup.tsx
@@ -0,0 +1,65 @@
+import styled from '@emotion/native';
+import {Txt, colors, Icon} from '@uoslife/design-system';
+import {TouchableOpacity} from 'react-native';
+type GuidePopupProps = {
+  children: React.ReactNode;
+  tail: 'LEFT' | 'CENTER' | 'RIGHT';
+  onPress: () => void;
+};
+const GuidePopup = ({children, tail, onPress}: GuidePopupProps) => {
+  return (
+    <GuidePopupContainer>
+      <GuidePopupBody>
+        <Txt
+          label={children ? children.toString() : ''}
+          color="grey190"
+          typograph="labelMedium"
+        />
+        <IconWrapper onPress={() => onPress()}>
+          <Icon color={'secondaryUi'} name={'clear'} width={24} height={24} />
+        </IconWrapper>
+      </GuidePopupBody>
+      <TriangleWrapper tail={tail}>
+        <Triangle />
+      </TriangleWrapper>
+    </GuidePopupContainer>
+  );
+};
+export default GuidePopup;
+
+const GuidePopupContainer = styled.View`
+  position: absolute;
+  bottom: 40px;
+`;
+
+const GuidePopupBody = styled.View`
+  flex-direction: row;
+  padding: 10px 8px 10px 16px;
+  border-radius: 30px;
+  align-items: center;
+  justify-content: center;
+  gap: 4px;
+  background: ${colors.secondaryBrand};
+`;
+
+type TailProps = {tail: 'LEFT' | 'CENTER' | 'RIGHT'};
+const TriangleWrapper = styled.View<TailProps>`
+  ${props =>
+    props.tail === 'LEFT'
+      ? 'align-items: flex-start; left:5%;'
+      : props.tail === 'RIGHT'
+      ? 'align-items: flex-end; right:8px;'
+      : 'align-items: center;'}
+  width: 100%;
+`;
+
+const Triangle = styled.View`
+  border-style: solid;
+  border-color: ${colors.secondaryBrand} transparent transparent transparent;
+  border-width: 24px 12px 0 12px;
+  height: 0;
+  width: 0;
+  top: -10px;
+`;
+type IconWrapperProps = {onPress: () => void};
+const IconWrapper = styled(TouchableOpacity)<IconWrapperProps>``;
--- src/screens/library/main_screen/MySeatScreen.tsx
+++ src/screens/library/main_screen/MySeatScreen.tsx
@@ -1,10 +1,13 @@
 import {useAtom} from 'jotai';
 import styled from '@emotion/native';
-import {Button, Txt, colors} from '@uoslife/design-system';
+import {Button, Txt, colors, Icon} from '@uoslife/design-system';
+import {useState} from 'react';
+import {TouchableOpacity} from 'react-native';
 
 import libraryReservationAtom from '../../../store/library';
 import LibraryUserInfo from '../../../components/molecules/screens/library/LibraryUserInfo';
 import useModal from '../../../hooks/useModal';
+import GuidePopup from '../../../components/molecules/common/GuidePopup';
 
 type Props = {redirectSeatList: () => void};
 
@@ -12,18 +15,31 @@ const MySeatScreen = ({redirectSeatList}: Props) => {
   const [{data}] = useAtom(libraryReservationAtom);
   const [openExtendModal, closeExtendModal, ExtendModal] = useModal('MODAL');
   const [openReturnModal, closeReturnModal, ReturnModal] = useModal('MODAL');
-  
+  const [isGuidePopupOpen, setIsGuidePopupOpen] = useState(false);
+
   const handleOnPressExtend = () => {
     console.error('좌석 연장 api 연결하기');
   };
   const handleOnPressReturn = () => {
     console.error('좌석 반납 api 연결하기');
   };
-  
+  const close = () => {
+    setIsGuidePopupOpen(false);
+  };
   return (
     <>
       <S.Container>
         <LibraryUserInfo />
+        <S.InformationWrapper>
+          {isGuidePopupOpen && (
+            <GuidePopup tail="RIGHT" onPress={close}>
+              포털 계정 연동하면 좌석을 발권할 수 있어요!
+            </GuidePopup>
+          )}
+          <S.InformationButtonWrapper onPress={() => setIsGuidePopupOpen(true)}>
+            <Icon color={'grey190'} name={'clear'} width={24} height={24} />
+          </S.InformationButtonWrapper>
+        </S.InformationWrapper>
         {data.reservationInfo ? (
           <S.ButtonWrapper>
             <Button
@@ -32,8 +48,12 @@ const MySeatScreen = ({redirectSeatList}: Props) => {
               isRounded
               onPress={openExtendModal}
             />
-            <Button label="좌석 반납하기" isFullWidth isRounded onPress={openReturnModal}/>
-
+            <Button
+              label="좌석 반납하기"
+              isFullWidth
+              isRounded
+              onPress={openReturnModal}
+            />
           </S.ButtonWrapper>
         ) : (
           <S.ButtonWrapper>
@@ -107,7 +127,8 @@ export default MySeatScreen;
 const S = {
   Container: styled.View`
     padding: 0 16px;
-    gap: 24px;
+    gap: 12px;
+    margin-top: 12px;
   `,
   ButtonWrapper: styled.View`
     width: 100%;
@@ -121,5 +142,17 @@ const S = {
     width: 100%;
     height: 1px;
     background-color: ${colors.grey40};
+  `,
+  InformationWrapper: styled.View`
+    align-items: flex-end;
+    right: 20px;
+  `,
+  InformationButtonWrapper: styled(TouchableOpacity)`
+    width: 40px;
+    height: 40px;
+    background: ${colors.secondaryBrand};
+    border-radius: 100px;
+    justify-content: center;
+    align-items: center;
   `,
 };
