79907950aacca76a5ed0b6eea97278859184d51b
alicehrk <81912837+alicehrk@users.noreply.github.com>
2024-04-21 01:18:05 +0900

    fix: API 연결 및 끌어당겨 새로고침 구현

--- src/screens/library/main_screen/ChallengeScreen.tsx
+++ src/screens/library/main_screen/ChallengeScreen.tsx
@@ -1,14 +1,22 @@
 import styled, {css} from '@emotion/native';
 import {Txt} from '@uoslife/design-system';
 import {View} from 'react-native';
+import {useQuery} from '@tanstack/react-query';
+import {RefreshControl} from 'react-native-gesture-handler';
+import {useEffect, useState} from 'react';
 import LibraryChallengeBoard from '../../../components/molecules/screens/library/LibraryChallengeBoard';
 import {
   ChallengeUserStatusDes1Enum,
   ChallengeUserStatusDes2Enum,
   ChallengeUserStatusEnum,
+  ChallengeUserStatusType,
 } from '../../../configs/utility/libraryChallenge/challengeUserStatus';
+import {UtilAPI} from '../../../api/services';
+import usePullToRefresh from '../../../hooks/usePullToRefresh';
 
-export const ChallengUserStatusImgEnum = {
+export const ChallengUserStatusImgEnum: {
+  [key in ChallengeUserStatusType]: any;
+} = {
   EGG: require('../../../assets/images/iroomae_egg.png'),
   BABY: require('../../../assets/images/iroomae_baby.png'),
   CHICK: require('../../../assets/images/iroomae_chick.png'),
@@ -18,13 +26,42 @@ export const ChallengUserStatusImgEnum = {
 };
 
 const ChallengeScreen = () => {
+  const {data: challengeData, refetch} = useQuery({
+    queryKey: ['getChallengeData', 'MONTH', 'UOSLIFE'],
+    queryFn: () =>
+      UtilAPI.getMyLibraryRanking({
+        duration: 'MONTH',
+        major: 'UOSLIFE',
+      }),
+  });
+
+  const {refreshing, onRefresh} = usePullToRefresh(refetch);
+
+  const [challengeStatus, setChallengeStatus] =
+    useState<ChallengeUserStatusType>('EGG');
+
+  useEffect(() => {
+    if (challengeData?.time != null) {
+      const {time} = challengeData;
+      if (time < 10) setChallengeStatus('EGG');
+      else if (time >= 10 && time < 20) setChallengeStatus('BABY');
+      else if (time >= 20 && time < 30) setChallengeStatus('CHICK');
+      else if (time >= 30 && time < 50) setChallengeStatus('JUNGDO');
+      else if (time >= 50 && time < 100) setChallengeStatus('CHEONJAE');
+      else if (time >= 100) setChallengeStatus('MANJAE');
+    }
+  }, [challengeData]);
+
   return (
-    <S.Container>
+    <S.Container
+      refreshControl={
+        <RefreshControl onRefresh={onRefresh} refreshing={refreshing} />
+      }>
       <S.CharacterBox>
-        <S.ImageContainer source={ChallengUserStatusImgEnum.BABY} />
+        <S.ImageContainer source={ChallengUserStatusImgEnum[challengeStatus]} />
         <S.TextBox>
           <Txt
-            label={ChallengeUserStatusEnum.BABY}
+            label={ChallengeUserStatusEnum[challengeStatus]}
             color="grey190"
             typograph="headlineMedium"
           />
@@ -35,19 +72,19 @@ const ChallengeScreen = () => {
               gap: 4px;
             `}>
             <Txt
-              label={ChallengeUserStatusDes1Enum.BABY}
+              label={ChallengeUserStatusDes1Enum[challengeStatus]}
               color="primaryBrand"
               typograph="titleSmall"
             />
             <Txt
-              label={ChallengeUserStatusDes2Enum.BABY}
+              label={ChallengeUserStatusDes2Enum[challengeStatus]}
               color="grey190"
               typograph="bodyMedium"
             />
           </View>
         </S.TextBox>
       </S.CharacterBox>
-      <LibraryChallengeBoard />
+      <LibraryChallengeBoard status={challengeStatus} />
     </S.Container>
   );
 };
@@ -55,19 +92,19 @@ const ChallengeScreen = () => {
 export default ChallengeScreen;
 
 const S = {
-  Container: styled.View`
+  Container: styled.ScrollView`
     width: 100%;
     height: 100%;
     background-color: #f7faff;
     padding-left: 28px;
     padding-right: 28px;
     padding-top: 20px;
-    gap: 20px;
   `,
   CharacterBox: styled.View`
     display: flex;
     align-items: center;
     gap: 8px;
+    margin-bottom: 20px;
   `,
   TextBox: styled.View`
     display: flex;
