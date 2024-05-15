4ff552a865805c0a437cc522425278a4b6f2c01a
EunBae Gong <geb9211@gmail.com>
2024-05-15 22:47:23 +0900

    feat: account api url 수정

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -5,5 +5,5 @@ export const DEFAULT_API_RETRY_LIMIT = 4;
 export const DEFAULT_API_RETRY_BACKOFFLIMIT = 0.5 * 초;
 
 export const BASE_API_URL = 'https://api.uoslife.com';
-export const ACCOUNT_API_URL = 'https://account.alpha.uoslife.com'; // TODO: 배포시 수정
+export const ACCOUNT_API_URL = 'https://account.uoslife.com';
 export const CDN_API_URL = 'https://cdn.uoslife.net';
