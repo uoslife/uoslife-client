13be5b6027aa3f6d05c87b079f344e7a6868f78c
alicehrk <81912837+alicehrk@users.noreply.github.com>
2024-06-23 21:32:33 +0900

    feat: ScheduleTab enum 추가

--- src/features/uos_lifestyle/academic_calendar/constants.ts
+++ src/features/uos_lifestyle/academic_calendar/constants.ts
@@ -4,3 +4,10 @@ export type ScheduleStatusType =
   | 'IN_PROGRESS'
   | 'ON_FILTER';
 export type ScheduleStatusEnumType = Record<ScheduleStatusType, string>;
+
+export type ScheduleTabType = 'ALL' | 'MY_SCHEDULE';
+export type ScheduleTabEnumType = Record<ScheduleTabType, string>;
+export const ScheduleTabEnum: ScheduleTabEnumType = {
+  ALL: '모든 일정',
+  MY_SCHEDULE: '내 일정',
+};
