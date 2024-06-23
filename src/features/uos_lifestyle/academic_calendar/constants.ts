00bb4ab4286523e1a760ac5bdce217fd79f34b20
alicehrk <81912837+alicehrk@users.noreply.github.com>
2024-06-23 21:26:08 +0900

    fix: 상수 선언 수정

--- src/features/uos_lifestyle/academic_calendar/constants.ts
+++ src/features/uos_lifestyle/academic_calendar/constants.ts
@@ -1,7 +1,6 @@
-export const STATUSES = [
-  'ALL',
-  'NOTIFICATION',
-  'IN_PROGRESS',
-  'ON_FILTER',
-] as const;
-export type Status = (typeof STATUSES)[number];
+export type ScheduleStatusType =
+  | 'ALL'
+  | 'NOTIFICATION'
+  | 'IN_PROGRESS'
+  | 'ON_FILTER';
+export type ScheduleStatusEnumType = Record<ScheduleStatusType, string>;
