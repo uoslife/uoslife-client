f7c04e24796c787e3c234cb7811d0501e9376265
EunBae Gong <76601773+eunbae0@users.noreply.github.com>
2024-07-03 16:15:42 +0900

    [RN-107] dev 모드에서 api url을 alpha로 변경 (#568)
    # Key Changes
    
    - dev 모드에서 api url을 alpha로 변경했습니다.
    
    # Closes Issue
    
    close #566

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -4,6 +4,8 @@ export const DEFAULT_API_TIMEOUT = 3 * 초;
 export const DEFAULT_API_RETRY_LIMIT = 4;
 export const DEFAULT_API_RETRY_BACKOFFLIMIT = 0.5 * 초;
 
-export const BASE_API_URL = 'https://api.uoslife.com';
-export const ACCOUNT_API_URL = 'https://account.uoslife.com';
+export const BASE_API_URL = `https://api.${__DEV__ ? 'alpha.' : ''}uoslife.com`;
+export const ACCOUNT_API_URL = `https://account.${
+  __DEV__ ? 'alpha.' : ''
+}uoslife.com`;
 export const CDN_API_URL = 'https://cdn.uoslife.net';
