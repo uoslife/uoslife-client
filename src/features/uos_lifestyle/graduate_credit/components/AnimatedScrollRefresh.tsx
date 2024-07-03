89ede49c2f5a7cb72a6aa3e377aeafa01ab69abe
youngchan Kim <aacz12035@gmail.com>
2024-07-03 22:16:03 +0900

    feat: 스크롤 애니메이션 및 리프레쉬 로직 추가

---
+++ src/features/uos_lifestyle/graduate_credit/components/AnimatedScrollRefresh.tsx
@@ -0,0 +1,201 @@
+import {View, PanResponder, StyleSheet} from 'react-native';
+import styled from '@emotion/native';
+import Animated, {
+  useAnimatedScrollHandler,
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withDelay,
+} from 'react-native-reanimated';
+import React, {useEffect, useState} from 'react';
+import {colors, Txt} from '@uoslife/design-system';
+import AnimatedPlayer from 'react-native-animated-webp';
+
+type AnimatedScrollRefreshComponentProps = {
+  children: React.ReactNode;
+  onScroll: () => void;
+};
+
+const AnimatedScrollRefreshComponent = ({
+  children,
+  onScroll,
+}: AnimatedScrollRefreshComponentProps) => {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const scrollPosition = useSharedValue(0);
+  const pullDownPosition = useSharedValue(0);
+  const isReadyToRefresh = useSharedValue(false);
+
+  useEffect(() => {
+    console.log(isRefreshing);
+    pullDownPosition.value = withTiming(0);
+  }, []);
+
+  const onRefresh = (done: () => void) => {
+    setIsRefreshing(true);
+    console.log('Refreshing...');
+    onScroll();
+    setTimeout(() => {
+      setIsRefreshing(false);
+      console.log('Refreshed!');
+      done();
+    }, 3000);
+  };
+
+  const onPanRelease = () => {
+    pullDownPosition.value = withTiming(isReadyToRefresh.value ? 70 : 0, {
+      duration: 180,
+    });
+
+    if (isReadyToRefresh.value) {
+      isReadyToRefresh.value = false;
+
+      // A function that resets the animation
+      const onRefreshComplete = () => {
+        pullDownPosition.value = withTiming(0, {duration: 180});
+      };
+
+      // trigger the refresh action
+      onRefresh(onRefreshComplete);
+    }
+  };
+
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: event => {
+      scrollPosition.value = event.contentOffset.y;
+    },
+  });
+  const panResponderRef = React.useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: (event, gestureState) =>
+        scrollPosition.value <= 0 && gestureState.dy >= 0,
+      onPanResponderMove: (event, gestureState) => {
+        const maxDistance = 70;
+        pullDownPosition.value = Math.max(
+          Math.min(maxDistance, gestureState.dy),
+          0,
+        );
+        if (
+          pullDownPosition.value >= maxDistance / 2 &&
+          !isReadyToRefresh.value
+        ) {
+          isReadyToRefresh.value = true;
+          console.log('Ready to refresh');
+        }
+
+        if (
+          pullDownPosition.value < maxDistance / 2 &&
+          isReadyToRefresh.value
+        ) {
+          isReadyToRefresh.value = false;
+          console.log('Will not refresh on release');
+        }
+      },
+      onPanResponderRelease: onPanRelease,
+      onPanResponderTerminate: onPanRelease,
+    }),
+  );
+  const pullDownStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: pullDownPosition.value,
+        },
+      ],
+    };
+  });
+  const refreshContainerStyles = useAnimatedStyle(() => {
+    return {
+      height: pullDownPosition.value,
+    };
+  });
+
+  const refreshIconStyles = useAnimatedStyle(() => {
+    const scale = Math.min(1, Math.max(0, pullDownPosition.value / 40));
+
+    return {
+      opacity: isRefreshing
+        ? withDelay(100, withTiming(0, {duration: 20}))
+        : Math.max(0, pullDownPosition.value) / 50,
+      transform: [
+        {
+          scaleX: isRefreshing ? withTiming(0.25, {duration: 120}) : scale,
+        },
+        {
+          scaleY: scale,
+        },
+      ],
+    };
+  }, [isRefreshing]);
+
+  const styles = StyleSheet.create({
+    root: {
+      flex: 1,
+      margin: 0,
+      padding: 0,
+    },
+    refreshContainer: {
+      position: 'absolute',
+      gap: 5,
+      top: -8,
+      left: 0,
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: colors.grey20,
+    },
+    refreshIcon: {
+      width: 28,
+      height: 28,
+      borderRadius: 18,
+      objectFit: 'contain',
+    },
+  });
+
+  return (
+    <View style={styles.root} pointerEvents={isRefreshing ? 'none' : 'auto'}>
+      <Animated.View style={[styles.refreshContainer, refreshContainerStyles]}>
+        {!isRefreshing ? (
+          <Animated.Image
+            source={require('../assets/scroll_refresh.png')}
+            style={[styles.refreshIcon, refreshIconStyles]}
+          />
+        ) : (
+          <AnimatedPlayer
+            thumbnailSource={require('../assets/animation/graduate_credit_scroll_refresh.gif')}
+            animatedSource={require('../assets/animation/graduate_credit_scroll_refresh.gif')}
+            style={{width: 60, height: 60}}
+          />
+        )}
+        <Txt
+          label={
+            isRefreshing
+              ? '최신 정보를 갖고 오고 있습니다'
+              : '아래로 당겨서 리프레쉬'
+          }
+          color="grey130"
+          typograph="labelLarge"
+        />
+      </Animated.View>
+      <Animated.View
+        style={[{flex: 1}, pullDownStyles]}
+        {...panResponderRef.current.panHandlers}>
+        <S.GraduateCreditScreen
+          onScroll={scrollHandler}
+          scrollEventThrottle={16}>
+          {children}
+        </S.GraduateCreditScreen>
+      </Animated.View>
+    </View>
+  );
+};
+
+export default AnimatedScrollRefreshComponent;
+
+const S = {
+  GraduateCreditScreen: styled(Animated.ScrollView)`
+    gap: 24px;
+    padding: 30px 16px 120px 16px;
+    flex: 1;
+  `,
+};
--- src/features/uos_lifestyle/graduate_credit/components/screens/GraduateCreditScreen.tsx
+++ src/features/uos_lifestyle/graduate_credit/components/screens/GraduateCreditScreen.tsx
@@ -16,6 +16,7 @@ import useUserState from '../../../../../hooks/useUserState';
 import {GraduateCreditRes} from '../../../../../api/services/core/graduateCredit/graduateCreditAPI.type';
 import {SUBJECT_BUTTON_LABEL} from '../../configs/constants';
 import {RootNavigationProps} from '../../../../../navigators/types/rootStack';
+import AnimatedScrollRefreshComponent from '../AnimatedScrollRefresh';
 
 const LoadingIndicatorComponent = () => (
   <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
@@ -101,170 +102,183 @@ const GraduateCreditScreen = () => {
     return <PortalUnauthorizedScreen />;
   }
 
-  if (isPendingForGetCredit || isPendingForCreateCredit) {
+  // 이수학점 생성 로딩 페이지
+  if (
+    isPendingForGetCredit ||
+    (isErrorForGetCredit && isPendingForCreateCredit)
+  ) {
     return <LoadingIndicatorComponent />;
   }
 
+  // 이수학점 페이지 랜더링
   return (
-    <ScrollView bounces={false}>
+    <View style={{flex: 1, backgroundColor: colors.white}}>
       <Header
         style={{paddingTop: inset.top, marginBottom: 8}}
         label="이수 학점 확인하기"
         onPressBackButton={() => navigation.goBack()}
       />
-      <S.GraduateCreditScreen>
-        <SubjectDetailButton
-          type="major"
-          label={`${user?.identity.department}`}
-        />
-        <S.HeaderView>
-          <Txt
-            label={`${user?.name}님은 졸업까지`}
-            color="grey190"
-            typograph="titleMedium"
+      <AnimatedScrollRefreshComponent onScroll={mutateGraduateCredit}>
+        <S.GraduateCreditScreen>
+          <SubjectDetailButton
+            type="major"
+            label={`${user?.identity.department}`}
           />
-          <S.FlexRowLayout>
+          <S.HeaderView>
             <Txt
-              label={`${
-                (graduateCreditData?.allCredit?.total ?? 0) -
-                (graduateCreditData?.allCredit?.current ?? 0)
-              }학점`}
-              color="primaryBrand"
-              typograph="headlineMedium"
+              label={`${user?.name}님은 졸업까지`}
+              color="grey190"
+              typograph="titleMedium"
             />
-            <Txt label="남았어요." color="grey190" typograph="headlineMedium" />
-          </S.FlexRowLayout>
-          <Txt
-            label={`${graduateCreditData?.allCredit?.current ?? 0}/${
-              graduateCreditData?.allCredit?.total ?? 0
-            }`}
-            color="grey130"
-            typograph="bodyMedium"
-          />
-        </S.HeaderView>
-        <S.ProgressBarContainer>
-          <ProgressBar
-            type="main"
-            maxNum={parseInt(`${graduateCreditData?.allCredit?.total ?? 0}`)}
-            currentCredit={parseInt(
-              `${graduateCreditData?.allCredit?.current ?? 0}`,
-            )}
-          />
-          <S.ProgressBarLabels>
-            <Txt label="0" color="grey60" typograph="labelMedium" />
-            <Txt label="130" color="grey60" typograph="labelMedium" />
-          </S.ProgressBarLabels>
-        </S.ProgressBarContainer>
-        <S.MinCreditView>
-          <S.FlexRowLayout>
-            {(graduateCreditData?.allCredit?.total ?? 0) >
-            (graduateCreditData?.allCredit?.current ?? 0) ? (
-              <>
-                <Icon color="grey130" name="info" width={20} height={20} />
-                <Txt
-                  label="아직 최소 학점을 채우지 못했어요"
-                  color="grey130"
-                  typograph="bodyMedium"
-                />
-              </>
-            ) : (
-              <>
-                <Icon
-                  color="primaryBrand"
-                  name="check"
-                  width={20}
-                  height={20}
-                />
-                <Txt
-                  label="졸업 학점을 모두 이수했어요"
-                  color="primaryBrand"
-                  typograph="bodyMedium"
-                />
-              </>
-            )}
-          </S.FlexRowLayout>
-          <S.FlexRowLayout>
-            {parsedResponse
-              ?.getRemainingSubect()
-              .map((field, index) => (
-                <SubjectDetailButton
-                  key={index}
-                  label={`${field.label as keyof typeof SUBJECT_BUTTON_LABEL}`}
-                  type="subject"
-                  data={graduateCreditData}
-                />
-              ))}
-          </S.FlexRowLayout>
-        </S.MinCreditView>
-        <S.HorizontalDividerThin />
-        <S.DetailCreditTagContainer>
-          {parsedResponse?.tags().map((tag, index) => (
-            <S.DetailCreditTag key={index}>
-              {tag.total !== 0 ? (
-                <S.TagWrapper>
-                  <S.TagHeader>
-                    <Txt
-                      label={tag.label}
-                      color="black"
-                      typograph="titleMedium"
-                    />
-                    <Pressable
-                      onPress={() =>
-                        navigation.navigate('graduate_credit_detail', {
-                          Props: graduateCreditData as ApiResponse,
-                          type: tag.label,
-                        })
-                      }>
+            <S.FlexRowLayout>
+              <Txt
+                label={`${
+                  (graduateCreditData?.allCredit?.total ?? 0) -
+                  (graduateCreditData?.allCredit?.current ?? 0)
+                }학점`}
+                color="primaryBrand"
+                typograph="headlineMedium"
+              />
+              <Txt
+                label="남았어요."
+                color="grey190"
+                typograph="headlineMedium"
+              />
+            </S.FlexRowLayout>
+            <Txt
+              label={`${graduateCreditData?.allCredit?.current ?? 0}/${
+                graduateCreditData?.allCredit?.total ?? 0
+              }`}
+              color="grey130"
+              typograph="bodyMedium"
+            />
+          </S.HeaderView>
+          <S.ProgressBarContainer>
+            <ProgressBar
+              type="main"
+              maxNum={parseInt(`${graduateCreditData?.allCredit?.total ?? 0}`)}
+              currentCredit={parseInt(
+                `${graduateCreditData?.allCredit?.current ?? 0}`,
+              )}
+            />
+            <S.ProgressBarLabels>
+              <Txt label="0" color="grey60" typograph="labelMedium" />
+              <Txt label="130" color="grey60" typograph="labelMedium" />
+            </S.ProgressBarLabels>
+          </S.ProgressBarContainer>
+          <S.MinCreditView>
+            <S.FlexRowLayout>
+              {(graduateCreditData?.allCredit?.total ?? 0) >
+              (graduateCreditData?.allCredit?.current ?? 0) ? (
+                <>
+                  <Icon color="grey130" name="info" width={20} height={20} />
+                  <Txt
+                    label="아직 최소 학점을 채우지 못했어요"
+                    color="grey130"
+                    typograph="bodyMedium"
+                  />
+                </>
+              ) : (
+                <>
+                  <Icon
+                    color="primaryBrand"
+                    name="check"
+                    width={20}
+                    height={20}
+                  />
+                  <Txt
+                    label="졸업 학점을 모두 이수했어요"
+                    color="primaryBrand"
+                    typograph="bodyMedium"
+                  />
+                </>
+              )}
+            </S.FlexRowLayout>
+            <S.FlexRowLayout>
+              {parsedResponse
+                ?.getRemainingSubect()
+                .map((field, index) => (
+                  <SubjectDetailButton
+                    key={index}
+                    label={`${
+                      field.label as keyof typeof SUBJECT_BUTTON_LABEL
+                    }`}
+                    type="subject"
+                    data={graduateCreditData}
+                  />
+                ))}
+            </S.FlexRowLayout>
+          </S.MinCreditView>
+          <S.HorizontalDividerThin />
+          <S.DetailCreditTagContainer>
+            {parsedResponse?.tags().map((tag, index) => (
+              <S.DetailCreditTag key={index}>
+                {tag.total !== 0 ? (
+                  <S.TagWrapper>
+                    <S.TagHeader>
+                      <Txt
+                        label={tag.label}
+                        color="black"
+                        typograph="titleMedium"
+                      />
+                      <Pressable
+                        onPress={() =>
+                          navigation.navigate('graduate_credit_detail', {
+                            Props: graduateCreditData as ApiResponse,
+                            type: tag.label,
+                          })
+                        }>
+                        <Icon
+                          name="forwardArrow"
+                          color="grey90"
+                          width={25}
+                          height={25}
+                        />
+                      </Pressable>
+                    </S.TagHeader>
+                    <S.TagFooter>
+                      <S.CreditInfo>
+                        <Txt
+                          label={`${tag.current}`}
+                          color="grey130"
+                          typograph="bodyMedium"
+                        />
+                        <Txt
+                          label={`/${tag.total}`}
+                          color="grey60"
+                          typograph="bodyMedium"
+                        />
+                      </S.CreditInfo>
+                      <S.StatusButton status={tag.status}>
+                        <S.StatusButtonText status={tag.status}>
+                          {tag.status ? '이수 완료' : '미이수'}
+                        </S.StatusButtonText>
+                      </S.StatusButton>
+                    </S.TagFooter>
+                  </S.TagWrapper>
+                ) : (
+                  <S.TagWrapper>
+                    <S.TagHeader>
+                      <Txt
+                        label={tag.label}
+                        color="grey40"
+                        typograph="titleMedium"
+                      />
                       <Icon
                         name="forwardArrow"
-                        color="grey90"
+                        color="grey40"
                         width={25}
                         height={25}
                       />
-                    </Pressable>
-                  </S.TagHeader>
-                  <S.TagFooter>
-                    <S.CreditInfo>
-                      <Txt
-                        label={`${tag.current}`}
-                        color="grey130"
-                        typograph="bodyMedium"
-                      />
-                      <Txt
-                        label={`/${tag.total}`}
-                        color="grey60"
-                        typograph="bodyMedium"
-                      />
-                    </S.CreditInfo>
-                    <S.StatusButton status={tag.status}>
-                      <S.StatusButtonText status={tag.status}>
-                        {tag.status ? '이수 완료' : '미이수'}
-                      </S.StatusButtonText>
-                    </S.StatusButton>
-                  </S.TagFooter>
-                </S.TagWrapper>
-              ) : (
-                <S.TagWrapper>
-                  <S.TagHeader>
-                    <Txt
-                      label={tag.label}
-                      color="grey40"
-                      typograph="titleMedium"
-                    />
-                    <Icon
-                      name="forwardArrow"
-                      color="grey40"
-                      width={25}
-                      height={25}
-                    />
-                  </S.TagHeader>
-                </S.TagWrapper>
-              )}
-            </S.DetailCreditTag>
-          ))}
-        </S.DetailCreditTagContainer>
-      </S.GraduateCreditScreen>
-    </ScrollView>
+                    </S.TagHeader>
+                  </S.TagWrapper>
+                )}
+              </S.DetailCreditTag>
+            ))}
+          </S.DetailCreditTagContainer>
+        </S.GraduateCreditScreen>
+      </AnimatedScrollRefreshComponent>
+    </View>
   );
 };
 
@@ -279,7 +293,6 @@ const S = {
   `,
   GraduateCreditScreen: styled.View`
     gap: 24px;
-    padding: 30px 16px 120px 16px;
     flex: 1;
   `,
   HeaderView: styled.View`
