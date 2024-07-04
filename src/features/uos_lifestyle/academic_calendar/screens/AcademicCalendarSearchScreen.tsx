217a83dc129561fe73dc2136c1696dc0e43dd877
Sungyeon Jung <yeonnybus@gmail.com>
2024-07-04 16:49:38 +0900

    fix: 백엔드에서 받아온 데이터에서 onClick 추가한 타입을 갖는 배열 추가 후 그걸 로드하는 로직 추가

--- src/features/uos_lifestyle/academic_calendar/screens/AcademicCalendarSearchScreen.tsx
+++ src/features/uos_lifestyle/academic_calendar/screens/AcademicCalendarSearchScreen.tsx
@@ -2,7 +2,7 @@ import {useNavigation} from '@react-navigation/native';
 import {View, ScrollView} from 'react-native';
 import {useState, ComponentProps, useRef, useEffect} from 'react';
 import styled from '@emotion/native';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
 import {TextInput} from 'react-native-gesture-handler';
 import {useQuery} from '@tanstack/react-query';
 import {Txt} from '@uoslife/design-system';
@@ -12,38 +12,42 @@ import SearchInput from '../../../../components/molecules/common/forms/searchInp
 import {ScheduleItemType} from '../types/ScheduleItemType';
 import CalendarAPI from '../api';
 import ScheduleItem from '../components/ScheduleItem';
+import Skeleton from '../../../../components/molecules/common/skeleton/Skeleton';
+import {ISchedule} from '../api/academicCalendarAPI.type';
 
 const AcademicCalendarSearchScreen = () => {
   const inset = useSafeAreaInsets();
   const navigation = useNavigation<RootNavigationProps>();
   const [searchWord, setSearchWord] = useState<string>('');
-  const [isSearchWordEntering, setSearchWordEntering] =
-    useState<boolean>(false);
   const inputRef = useRef<TextInput>(null);
   const [triggerSearch, setTriggerSearch] = useState<boolean>(false);
+  const [uiItem, setUiItem] = useState<ScheduleItemType[]>([]);
 
   const searchInputProps: ComponentProps<typeof SearchInput> = {
     inputRef,
     placeholder: '일정을 검색하세요.',
     onFocus: () => {
-      setSearchWordEntering(true);
       inputRef.current?.focus();
     },
     onChangeText: text => {
       setSearchWord(text);
+      setTriggerSearch(false);
     },
     onSubmitEditing: () => {
       setTriggerSearch(true);
     },
     onPressClear: () => {
       setSearchWord('');
-      setSearchWordEntering(true);
       inputRef.current?.focus();
     },
     value: searchWord,
   };
 
-  const {data: getMyscheduleItem, isSuccess} = useQuery<ScheduleItemType[]>({
+  const {
+    data: getMyscheduleItem,
+    isSuccess,
+    isFetching,
+  } = useQuery<ISchedule[]>({
     queryKey: ['SearchResults', searchWord],
     queryFn: async () =>
       await CalendarAPI.getSearchedSchedule({keyword: searchWord}),
@@ -51,26 +55,41 @@ const AcademicCalendarSearchScreen = () => {
   });
   useEffect(() => {
     if (isSuccess) {
-      setTriggerSearch(false);
+      // backend data => front end data
+      const bookmarkOnClick = () => {
+        console.log('bookmarked!');
+      };
+      const ui: ScheduleItemType[] = getMyscheduleItem.map(item => {
+        const tmp = {...item, onClick: bookmarkOnClick};
+        return tmp;
+      });
+      setUiItem(ui);
     }
   }, [isSuccess]);
 
   return (
-    <View style={{paddingBottom: inset.bottom}}>
+    <SafeAreaView style={{paddingBottom: inset.bottom, flex: 1}}>
       <Header
-        style={{paddingTop: inset.top, marginBottom: 8}}
+        style={{marginBottom: 8}}
         label=""
         onPressBackButton={() => navigation.goBack()}>
         <SearchInput {...searchInputProps} />
       </Header>
       <S.Container>
-        {isSuccess && getMyscheduleItem && triggerSearch && (
+        {isFetching && (
           <ScrollView
             contentContainerStyle={{
               padding: 16,
-              paddingBottom: inset.top + 150,
             }}>
-            {getMyscheduleItem.length === 0 ? (
+            <Skeleton variant="card" />
+          </ScrollView>
+        )}
+        {isSuccess && triggerSearch && (
+          <ScrollView
+            contentContainerStyle={{
+              padding: 16,
+            }}>
+            {uiItem.length === 0 ? (
               <S.TxtContainer>
                 <Txt
                   label="검색결과가 없어요."
@@ -79,18 +98,16 @@ const AcademicCalendarSearchScreen = () => {
                 />
               </S.TxtContainer>
             ) : (
-              getMyscheduleItem.map((item, index) => (
+              uiItem.map((item, index) => (
                 <S.ItemContainer>
                   <ScheduleItem
                     schedule={item}
+                    onCheckboxChange={() => {
+                      console.log('f');
+                    }}
+                    isChecked={false}
                     editable={false}
                     checkedIdx={index}
-                    isChecked={false}
-                    onCheckboxChange={(id, isChecked) => {
-                      console.log(
-                        `Checkbox 상태 변경: ID=${id}, isChecked=${isChecked}`,
-                      );
-                    }}
                   />
                 </S.ItemContainer>
               ))
@@ -98,7 +115,7 @@ const AcademicCalendarSearchScreen = () => {
           </ScrollView>
         )}
       </S.Container>
-    </View>
+    </SafeAreaView>
   );
 };
 
